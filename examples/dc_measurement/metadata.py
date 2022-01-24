30e66ae62b529a9b498b350313df122a46a3faac
Vasantha-Kumar-J <vasanthakumar2598@gmail.com>
2022-01-24 18:50:07 +0530

    Updated multiline comments to single line comments in metadata.py

--- examples/dc_measurement/metadata.py
+++ examples/dc_measurement/metadata.py
@@ -1,22 +1,20 @@
-"""
-Measurement Metadata
-"""
+# Measurement Metadata
 DISPLAY_NAME = "DC Measurement(Py)"
 VERSION = "0.0.1"
 MEASUREMENT_TYPE = "DC"
 PRODUCT_TYPE = "ADC"
 SCREEN_FILE_NAME = "DCMeasurementScreen.isscr"
+
+# Measurement Modules and Method Info
 MEASUREMENT_MODULE_NAME = "measurement"
 MEASUREMENT_METHOD_NAME = "measure"
 
-"""
-Instrument Info
-"""
+
+# Instrument Info
 RESOURCE_NAME = "DPS_4145"
 
-"""
-Service Info
-"""
+
+# Service Info
 SERVICE_CLASS = "DCMeasurement_Python"
 SERVICE_ID = "{B290B571-CB76-426F-9ACC-5168DC1B027C}"
 DESCRIPTION_URL = "https://www.ni.com/measurementservices/dcmeasurement.html"
