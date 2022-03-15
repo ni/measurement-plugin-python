51a7de27fb68a19be224e44bffa58b428331d6da
Vasantha-Kumar-J <vasanthakumar2598@gmail.com>
2022-03-15 09:34:04 +0530

    Add parameter serializer with serialise and deserialise

--- source/measurement_services_core/parameter_serializer.py
+++ source/measurement_services_core/parameter_serializer.py
@@ -1,6 +1,8 @@
+import io
 from typing import ByteString, Dict, Iterable
 import serializationstrategy
 import google.protobuf.type_pb2 as type_pb2
+from google.protobuf.internal import encoder
 
 
 class Parameter:
@@ -17,12 +19,40 @@ class ParameterMetadata:
 
 
 def deserialize_parameters(
-    parametersMetadata: Dict[id, ParameterMetadata], parametersByteArray: ByteString
+    parameter_metadata_dict: Dict[id, ParameterMetadata], parameters_byte_array: ByteString
 ) -> Iterable[Parameter]:
-    pass
+
+    position = 0
+    mapping = {}
+    parameter_list = []
+    for parameter_info in parameter_metadata_dict:
+        type = parameter_info.value.type
+        is_repeated = parameter_info.value.repeated
+        field_index = parameter_info.key
+        serializer = serializationstrategy.Context.get_strategy(type, is_repeated)
+        decoder = serializer.decoder(field_index, field_index)
+        position = decoder(
+            parameters_byte_array,
+            position + encoder._TagSize(field_index),
+            parameters_byte_array.__sizeof__(),
+            type,
+            mapping,
+        )
+    for item in mapping:
+        parameter = Parameter(item.key, item.value)
+        parameter_list.append(parameter)
+    return parameter_list
 
 
 def serialize_parameters(
-    parameter_metadata: Dict[id, ParameterMetadata], parameterValue: Iterable[Parameter]
+    parameter_metadata_dict: Dict[id, ParameterMetadata], parameter_value: Iterable[Parameter]
 ) -> ByteString:
-    pass
+    serialize_buffer = io.BytesIO()
+    for parameter in parameter_value:
+        serializer = serializationstrategy.Context.get_strategy(
+            parameter_metadata_dict[parameter.id].type,
+            parameter_metadata_dict[parameter.id].repeated,
+        )
+        encoder = serializer.encoder(parameter.id)
+        encoder(serialize_buffer, parameter.value)
+    return serialize_buffer.getvalue()
