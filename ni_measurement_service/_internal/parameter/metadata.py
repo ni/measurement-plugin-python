ef5384466fe5ba845f26b0da79bfb4350c90d5be
Vasantha-Kumar-J <vasanthakumar2598@gmail.com>
2022-03-22 14:43:01 +0530

    Validate if the default value is matching the value provided.

--- ni_measurement_service/_internal/parameter/metadata.py
+++ ni_measurement_service/_internal/parameter/metadata.py
@@ -2,6 +2,7 @@
 from typing import Any, NamedTuple
 
 import google.protobuf.type_pb2 as type_pb2
+from ni_measurement_service._internal.parameter.serializationstrategy import Context
 
 
 class ParameterMetadata(NamedTuple):
@@ -22,3 +23,39 @@ class ParameterMetadata(NamedTuple):
     type: type_pb2.Field
     repeated: bool
     default_value: Any
+
+
+def validate_default_value_type(parameter_metadata: ParameterMetadata) -> None:
+    """Validate and raise expection if the default value is not matching the type info.
+
+    Args
+    ----
+        parameter_metadata (ParameterMetadata): Parameter metadata
+
+    Raises
+    ------
+        TypeError: If default value is not matching the Datatype.
+
+    """
+    display_name = parameter_metadata.display_name
+    default_value = parameter_metadata.default_value
+    if default_value is None:
+        return None
+
+    expected_type = type(
+        Context.get_type_default(parameter_metadata.type, parameter_metadata.repeated)
+    )
+
+    if type(default_value) != expected_type:
+        raise TypeError(
+            f"Default value for '{display_name}' cannot be of type: {type(default_value)}.Expected type: {expected_type}."
+        )
+
+    if parameter_metadata.repeated:
+        expected_element_type = type(Context.get_type_default(parameter_metadata.type, False))
+        for element in default_value:
+            if type(element) != expected_element_type:
+                raise TypeError(
+                    f"Default value list for '{display_name}' cannot contain an element of type: {type(element)}.Expected type for all elements: {expected_type}."
+                )
+    return None
