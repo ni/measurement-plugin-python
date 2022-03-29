a225390c97b5d609c403cf182e71963cc0579cd4
Vasantha-Kumar-J <vasanthakumar2598@gmail.com>
2022-03-29 19:51:07 +0530

    Add comment on noqa in __init__.py

--- ni_measurement_service/__init__.py
+++ ni_measurement_service/__init__.py
@@ -1,6 +1,6 @@
 """National Instruments Framework for hosting Python Measurement Services."""
-from .measurement.info import DataType  # noqa F401
-from .measurement.info import MeasurementInfo  # noqa F401
-from .measurement.info import ServiceInfo  # noqa F401
-from .measurement.info import UIFileType  # noqa F401
-from .measurement.service import MeasurementService  # noqa F401
+from .measurement.info import DataType  # noqa F401, declaring API
+from .measurement.info import MeasurementInfo  # noqa F401, declaring API
+from .measurement.info import ServiceInfo  # noqa F401, declaring API
+from .measurement.info import UIFileType  # noqa F401, declaring API
+from .measurement.service import MeasurementService  # noqa F401, declaring API
