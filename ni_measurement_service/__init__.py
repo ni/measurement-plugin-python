bd169fc00a1351c439bad26f2e33746ded95df52
Vasantha-Kumar-J <vasanthakumar2598@gmail.com>
2022-03-29 10:14:17 +0530

    Added noqa for __init__.py file

--- ni_measurement_service/__init__.py
+++ ni_measurement_service/__init__.py
@@ -1,6 +1,6 @@
 """National Instruments Framework for hosting Python Measurement Services."""
-from .measurement.info import DataType
-from .measurement.info import MeasurementInfo
-from .measurement.info import ServiceInfo
-from .measurement.info import UIFileType
-from .measurement.service import MeasurementService
+from .measurement.info import DataType  # noqa F401
+from .measurement.info import MeasurementInfo  # noqa F401
+from .measurement.info import ServiceInfo  # noqa F401
+from .measurement.info import UIFileType  # noqa F401
+from .measurement.service import MeasurementService  # noqa F401
