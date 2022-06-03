c5da045d39b921001c381979f0ac8a75b4199b8f
Vasantha-Kumar-J <vasanthakumar2598@gmail.com>
2022-06-03 17:12:30 +0530

    Update examples with start bat and service config files.

--- examples/dc_measurement_screenUI/measurement.py
+++ examples/dc_measurement_screenUI/measurement.py
@@ -25,7 +25,7 @@ measurement_info = nims.MeasurementInfo(
 
 service_info = nims.ServiceInfo(
     service_class="DCMeasurement_Python",
-    service_id="{B290B571-CB76-426F-9ACC-5168DC1B027C}",
+    service_id="{825076C9-0484-4123-B360-81D29397824F}",
     description_url="https://www.ni.com/measurementservices/dcmeasurement.html",
 )
 
@@ -72,7 +72,9 @@ def measure(
         measured_value = None
         with session.initiate():
             channel = session.get_channel_names("0")
-            measured_value = session.channels[channel].fetch_multiple(count=1, timeout=timeout)
+            measured_value = session.channels[channel].fetch_multiple(
+                count=1, timeout=timeout
+            )
     print_fetched_measurements(measured_value)
     measured_voltage = measured_value[0].voltage
     measured_current = measured_value[0].current
