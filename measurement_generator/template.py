0b2ab3c68009b83555717f486b36e81d98af72f8
Warren Favors <warren.favors@ni.com>
2022-07-18 13:43:53 -0500

    Add unittest and poetry

--- measurement_generator/template.py
+++ measurement_generator/template.py
@@ -1,98 +1,114 @@
-import click
+import pathlib
 import re
+import unittest
 import uuid
-import os.path
+
+import click
+from mako import exceptions
 from mako.template import Template
 
 
-def create_py(name, vers, m_type, p_type, ui, s_class, id, desc):
-    py = open(
-        os.path.join(os.path.dirname(os.path.abspath(__file__)), f"{name}.py"), "w"
-    )
+def _render_template(template_name: str, **template_args):
+    try:
+        template = Template(filename=str(pathlib.Path(__file__).parent / template_name))
+        return template.render(**template_args)
+    except:  # noqa: E722
+        print(exceptions.text_error_template().render())
 
-    template = Template(
-        filename=os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), "pyTemplate.txt"
+
+def _read_file(file_name):
+    file_path = (
+        pathlib.Path(__file__).parent.absolute()
+        / f"example_renders/{file_name}"
+    )
+    with file_path.open("r") as fout:
+        return fout.read()
+
+
+class TestRender(unittest.TestCase):
+    def test_py_render(self):
+        self.assertEqual(
+            _render_template(
+                "templates/pyTemplate.txt.mako",
+                display_name="SampleMeasurement",
+                version="1.0.0",
+                measurement_type="Sample",
+                product_type="Sample",
+                ui_file="measurementUI.measui",
+                service_class="SampleMeasurement_Python",
+                service_id="{E0095551-CB4B-4352-B65B-4280973694B2}",
+                description="description",
+            ),
+            _read_file("example_py.txt"),
         )
-    )
-    py.write(
-        template.render(
-            display_name=name,
-            version=vers,
-            measurement_type=m_type,
-            product_type=p_type,
-            ui_file=ui,
-            service_class=s_class,
-            service_id=id,
-            description=desc,
+
+    def test_sc_render(self):
+        self.assertEqual(
+            _render_template(
+                "templates/scTemplate.txt.mako",
+                display_name="SampleMeasurement",
+                service_class="SampleMeasurement_Python",
+                service_id="{E0095551-CB4B-4352-B65B-4280973694B2}",
+                description="description",
+            ),
+            _read_file("example_serviceconfig.txt"),
         )
-    )
-    py.close()
 
 
-def create_serviceConfig(name, s_class, id, desc):
-    sc = open(
-        os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), f"{name}.serviceConfig"), "w"
-    )
+def _create_file(template_name: str, file_name: str, **template_args) -> str:
+    output_file = pathlib.Path(__file__).parent / file_name
 
-    template = Template(
-        filename=os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), "scTemplate.txt"
-        )
-    )
-    sc.write(
-        template.render(
-            display_name=name, service_class=s_class, service_id=id, description=desc
-        )
-    )
-    sc.close()
+    output = _render_template(template_name, **template_args)
+
+    with output_file.open("w") as fout:
+        fout.write(output)
 
 
-def create_bat(name):
-    bat = open(
-        os.path.join(os.path.dirname(os.path.abspath(__file__)), f"run{name}.bat"), "w"
-    )
-    py_file_path = os.path.join(
-        os.path.dirname(os.path.abspath(__file__)), f"{name}.py"
-    )
-    bat.write(f"call python {py_file_path}")
-    bat.close()
+def _create_bat(name):
+    output_file = pathlib.Path(__file__).parent / f"run_{name}.bat"
+
+    py_file_path = str(pathlib.Path(__file__).parent.absolute() / f"{name}.py")
+
+    with output_file.open("w") as fout:
+        fout.write(f"call python {py_file_path}")
 
 
-def check_version(version):
-    pattern = "^[0-9]+\.[0-9]+\.[0-9]+$"
-    if not re.search(pattern, version):
-        raise ValueError("version not entered correctly")
+def _check_version(ctx, param, version):
+    pattern = r"^[0-9]+\.[0-9]+\.[0-9]+$"  # ex: 2.6.8
+    if re.match(pattern, version):
+        return version
+    raise ValueError("version not entered correctly")
 
 
-def check_ui(ui_file):
-    if ui_file != "" and not os.path.exists(
-        os.path.join(os.path.dirname(os.path.abspath(__file__)), ui_file)
+def _check_ui(ctx, param, ui_file):
+    if ui_file != "" and not pathlib.Path.exists(
+        pathlib.Path(__file__).parent.absolute() / ui_file
     ):
         raise ValueError("can't find UI file")
+    return ui_file
 
 
-def create_service_class(service_class, display_name):
+def _create_service_class(service_class, display_name):
     if service_class == "":
         return f"{display_name}_Python"
     else:
         return service_class
 
 
-def check_uuid4(test_uuid, version=4):
+def _check_uuid(test_uuid):
     try:
-        return uuid.UUID(test_uuid).version == version
+        uuid.UUID(test_uuid)
+        return True
     except ValueError:
         return False
 
 
-def create_guid(service_id):
+def _check_guid(ctx, param, service_id):
     if service_id == "":
         return "{" + str(uuid.uuid4()) + "}"
     else:
         service_id = service_id.replace("{", "").replace("}", "")
-        if check_uuid4(service_id):
+        if _check_uuid(service_id):
             return "{" + service_id + "}"
         raise ValueError("GUID not entered correctly")
 
@@ -100,32 +116,36 @@ def create_guid(service_id):
 # Takes in command line arguments to create a .py file, a .serviceConfig file, and a .bat file.
 @click.command()
 @click.argument("display_name")
-@click.argument("version")
+@click.argument("version", callback=_check_version)
 @click.argument("measurement_type")
 @click.argument("product_type")
 @click.option(
-    "-UI", 
-    "--ui_file", 
-    default="", 
-    help="Name of the UI File")
+    "-U",
+    "--ui-file",
+    default="",
+    help="Name of the UI File",
+    callback=_check_ui,
+)
 @click.option(
-    "-SC",
-    "--service_class",
+    "-S",
+    "--service-class",
     default="",
     help="Service Class that the measurement belongs to",
 )
 @click.option(
-    "-ID", 
-    "--service_id", 
-    default="", 
-    help="Unique GUID")
+    "-I",
+    "--service-id",
+    default="",
+    help="Unique GUID",
+    callback=_check_guid,
+)
 @click.option(
-    "-DE",
+    "-D",
     "--description",
     default="",
     help="Description URL that contains information about the measurement",
 )
-def create_template(
+def _create_measurement(
     display_name,
     version,
     measurement_type,
@@ -135,24 +155,34 @@ def create_template(
     service_id,
     description,
 ):
-    check_version(version)
-    check_ui(ui_file)
-    service_class = create_service_class(service_class, display_name)
-    service_id = create_guid(service_id)
+    service_class = _create_service_class(service_class, display_name)
 
-    create_py(
-        display_name,
-        version,
-        measurement_type,
-        product_type,
-        ui_file,
-        service_class,
-        service_id,
-        description,
+    _create_file(
+        "templates/pyTemplate.txt.mako",
+        f"{display_name}.py",
+        display_name=display_name,
+        version=version,
+        measurement_type=measurement_type,
+        product_type=measurement_type,
+        ui_file=ui_file,
+        service_class=service_class,
+        service_id=service_id,
+        description=description,
     )
-    create_serviceConfig(display_name, service_class, service_id, description)
-    create_bat(display_name)
+    _create_file(
+        "templates/scTemplate.txt.mako",
+        f"{display_name}.serviceConfig",
+        display_name=display_name,
+        service_class=service_class,
+        service_id=service_id,
+        description=description,
+    )
+    _create_bat(display_name)
+
+
+def main():
+    _create_measurement()
 
 
 if __name__ == "__main__":
-    create_template()
+    main()
---
+++ measurement_generator/templates/pyTemplate.txt.mako
@@ -0,0 +1,54 @@
+<%page args="display_name, version, measurement_type, product_type, ui_file, service_class, service_id, description"/>\
+\
+import logging
+import os
+import sys
+
+import click
+import ni_measurement_service as nims
+
+measurement_info = nims.MeasurementInfo(
+    display_name="${display_name}",
+    version="${version}",
+    measurement_type="${measurement_type}",
+    product_type="${product_type}",
+    ui_file_path=os.path.join(os.path.dirname(os.path.abspath(__file__)), "${ui_file}"),
+    ui_file_type=nims.UIFileType.MeasurementUI,
+)
+
+service_info = nims.ServiceInfo(
+    service_class="${service_class}",
+    service_id="${service_id}",
+    description_url="${description}",
+)
+
+measurement_service = nims.MeasurementService(measurement_info, service_info)
+
+@measurement_service.register_measurement
+@measurement_service.configuration("Array in", nims.DataType.DoubleArray1D, [0.0])
+@measurement_service.output("Array out", nims.DataType.DoubleArray1D)
+def measure(array_input):
+    array_output = array_input
+    return [array_output]
+
+@click.command
+@click.option(
+    "-v", "--verbose", count=True, help="Enable verbose logging. Repeat to increase verbosity."
+)
+def main(verbose: int):
+    """Host the Sample Measurement service."""
+    if verbose > 1:
+        level = logging.DEBUG
+    elif verbose == 1:
+        level = logging.INFO
+    else:
+        level = logging.WARNING
+    logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s", level=level)
+
+    measurement_service.host_service()
+    input("Press enter to close the measurement service.\n")
+    measurement_service.close_service()
+
+
+if __name__ == "__main__":
+    sys.exit(main())
