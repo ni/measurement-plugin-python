784d03a72670c22ba12a6845019528cb9d3efb94
wfavors <109159954+wfavors@users.noreply.github.com>
2022-08-12 14:26:55 -0500

    Users/wfavors/add measurement generator (#73)
    * Add python measurement template generator
    
    * Add unittest and poetry
    
    * Remove string typecast in template.py
    
    Co-authored-by: mshafer-NI <matthew.shafer@ni.com>
    
    * Remove string interpolation in template.py
    
    Co-authored-by: Brad Keryan <brad.keryan@ni.com>
    
    * Add pytest and documentation
    
    * Change test function docstring
    
    Co-authored-by: mshafer-NI <matthew.shafer@ni.com>
    
    * Change documentation and example renders
    
    * Change UI file parameter docstring
    
    * Remove stale poetry.lock
    
    * Add testing for batch file generation
    
    * Fix name example.bat_
    
    * Remove example.bat.bat
    
    * Add template.py use instructions to README
    
    * Reorganize measurement generator file structure
    
    * Change working directory instruction in README.md
    
    Co-authored-by: mshafer-NI <matthew.shafer@ni.com>
    
    * Add measurement generator description to pyproject.toml
    
    Co-authored-by: mshafer-NI <matthew.shafer@ni.com>
    
    * add __main__.py
    
    * update workflows to handle generator
    
    * Fix lock change checking in workflows
    
    * List "ni-measurement-generator" script in pyproj
    
    Co-authored-by: mshafer-NI <matthew.shafer@ni.com>
    
    * Remove echo in PR.yml
    
    * Ran poetry lock
    
    * Add filtering for just running tests on measurement_service
    
    Co-authored-by: mshafer-NI <matthew.shafer@ni.com>
    
    * Fix lint errors
    
    * Fix more lint errors
    
    * Fix more lint errors
    
    * Fix test coverage
    
    * Add poetry install for generator
    
    Co-authored-by: mshafer-NI <matthew.shafer@ni.com>
    Co-authored-by: Brad Keryan <brad.keryan@ni.com>

---
+++ ni_measurement_generator/ni_measurement_generator/example_renders/example.py
@@ -0,0 +1,61 @@
+"""A default measurement with an array in and out."""
+import logging
+import os
+import sys
+
+import click
+
+import ni_measurement_service as nims
+
+measurement_info = nims.MeasurementInfo(
+    display_name="SampleMeasurement",
+    version="1.0.0.0",
+    measurement_type="Measurement",
+    product_type="Product",
+    ui_file_path=os.path.join(os.path.dirname(os.path.abspath(__file__)), "MeasurementUI.measui"),
+    ui_file_type=nims.UIFileType.MeasurementUI,
+)
+
+service_info = nims.ServiceInfo(
+    service_class="SampleMeasurement_Python",
+    service_id="{E0095551-CB4B-4352-B65B-4280973694B2}",
+    description_url="description",
+)
+
+measurement_service = nims.MeasurementService(measurement_info, service_info)
+
+
+@measurement_service.register_measurement
+@measurement_service.configuration("Array in", nims.DataType.DoubleArray1D, [0.0])
+@measurement_service.output("Array out", nims.DataType.DoubleArray1D)
+def measure(array_input):
+    """TODO: replace the following line with your own measurement logic."""
+    array_output = array_input
+    return [array_output]
+
+
+@click.command
+@click.option(
+    "-v",
+    "--verbose",
+    count=True,
+    help="Enable verbose logging. Repeat to increase verbosity.",
+)
+def main(verbose: int):
+    """Host the Sample Measurement service."""
+    if verbose > 1:
+        level = logging.DEBUG
+    elif verbose == 1:
+        level = logging.INFO
+    else:
+        level = logging.WARNING
+    logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s", level=level)
+
+    measurement_service.host_service()
+    input("Press enter to close the measurement service.\n")
+    measurement_service.close_service()
+
+
+if __name__ == "__main__":
+    main()
+    sys.exit(0)
---
+++ ni_measurement_generator/ni_measurement_generator/template.py
@@ -0,0 +1,175 @@
+"""Utilizes command line args to create a measurement using template files."""
+import pathlib
+import re
+import uuid
+
+import click
+from mako import exceptions
+from mako.template import Template
+
+
+def _render_template(template_name: str, **template_args) -> str:
+    file_path = str(pathlib.Path(__file__).parent / "templates" / template_name)
+
+    with open(file_path, "r") as file:
+        file_contents = file.read()
+    template = Template(file_contents)
+    try:
+        return template.render(**template_args)
+    except:  # noqa: E722
+        print(exceptions.text_error_template().render())
+
+
+def _create_file(template_name: str, file_name: str, directory_out, **template_args) -> str:
+    output_file = pathlib.Path(directory_out) / file_name
+
+    output = _render_template(template_name, **template_args)
+
+    with output_file.open("w") as fout:
+        fout.write(output)
+
+
+def _create_bat(directory_out):
+    output_file = pathlib.Path(directory_out) / "start.bat"
+
+    with output_file.open("w") as fout:
+        fout.write(f"call python %~dp0measurement.py")
+
+
+def _check_version(ctx, param, version):
+    pattern = r"^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$"
+    if re.match(pattern, version):
+        return version
+    raise ValueError("version not entered correctly")
+
+
+def _check_ui(ui_file):
+    if ui_file != "" and not pathlib.Path.exists(
+        pathlib.Path(__file__).parent.absolute() / ui_file
+    ):
+        raise ValueError("can't find UI file")
+
+
+def _get_ui_type(ui_file):
+    ext = pathlib.Path(ui_file).suffix
+    if ext == ".measui":
+        return "MeasurementUI"
+    elif ext == ".vi":
+        return "LabVIEW"
+    else:
+        raise ValueError(
+            "UI file extension does not match possible UI file types. Should be .measui or .vi."
+        )
+
+
+def _resolve_ui_file(ui_file, display_name):
+    if ui_file is None:
+        return f"{display_name}.measui"
+    else:
+        _check_ui(ui_file)
+        return ui_file
+
+
+def _resolve_service_class(service_class, display_name):
+    if service_class is None:
+        return f"{display_name}_Python"
+    else:
+        return service_class
+
+
+def _check_guid(ctx, param, service_id):
+    try:
+        parsed_id = uuid.uuid4() if service_id is None else uuid.UUID(service_id)
+        return "{" + str(parsed_id).upper() + "}"
+    except ValueError:
+        raise ValueError(f"The {param} must be a valid GUID.")
+
+
+@click.command()
+@click.argument("display_name")
+@click.argument("version", callback=_check_version)
+@click.argument("measurement_type")
+@click.argument("product_type")
+@click.option(
+    "-u",
+    "--ui-file",
+    help="Name of the UI File, Default is <display_name>.measui.",
+)
+@click.option(
+    "-s",
+    "--service-class",
+    help="Service Class that the measurement belongs to. Default is <display_name>_Python.",
+)
+@click.option(
+    "-i",
+    "--service-id",
+    help="Unique GUID.",
+    callback=_check_guid,
+)
+@click.option(
+    "-d",
+    "--description",
+    help="Description URL that contains information about the measurement.",
+)
+@click.option(
+    "-o",
+    "--directory-out",
+    default=pathlib.Path(__file__).parent,
+    help="Output directory for measurement files.",
+)
+def create_measurement(
+    display_name,
+    version,
+    measurement_type,
+    product_type,
+    ui_file,
+    service_class,
+    service_id,
+    description,
+    directory_out,
+):
+    """Generate a Python measurement service from a template.
+
+    You can use this to get started writing your own measurement services.
+
+    DISPLAY_NAME: The measurement display name for client to display to user.
+    The created .serviceconfig file will take this as its file name.
+
+    VERSION: The measurement version that helps to maintain versions of a measurement in future.
+    Should be formatted like x.x.x.x
+    e.g. 0.2.6.8
+
+    MEASUREMENT_TYPE: Represents category of measurement for the ProductType,
+    e.g. AC or DC measurements.
+
+    PRODUCT_TYPE: Represents type of the DUT,
+    e.g. ADC, LDO.
+    """
+    service_class = _resolve_service_class(service_class, display_name)
+    ui_file = _resolve_ui_file(ui_file, display_name)
+    ui_file_type = _get_ui_type(ui_file)
+
+    _create_file(
+        "measurement.py.mako",
+        "measurement.py",
+        directory_out,
+        display_name=display_name,
+        version=version,
+        measurement_type=measurement_type,
+        product_type=product_type,
+        ui_file=ui_file,
+        ui_file_type=ui_file_type,
+        service_class=service_class,
+        service_id=service_id,
+        description=description,
+    )
+    _create_file(
+        "measurement.serviceconfig.mako",
+        f"{display_name}.serviceconfig",
+        directory_out,
+        display_name=display_name,
+        service_class=service_class,
+        service_id=service_id,
+        description=description,
+    )
+    _create_bat(directory_out)
