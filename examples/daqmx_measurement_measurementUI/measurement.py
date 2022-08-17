0c4ded4507f9dc261e9f247e167a21d1e357606b
Wesley Tang <wesley.tang@ni.com>
2022-08-17 12:24:25 -0500

    Added initial implementation of measurement.py for nidaqmx example. Untested.

---
+++ examples/daqmx_measurement_measurementUI/measurement.py
@@ -0,0 +1,91 @@
+"""User Measurement.
+
+User can Import driver and 3rd Party Packages based on requirements.
+
+"""
+
+import logging
+import os
+import sys
+
+import click
+import hightime
+import nidaqmx
+from nidaqmx import constants
+
+import ni_measurement_service as nims
+
+
+measurement_info = nims.MeasurementInfo(
+    display_name="DAQmxMeasurement(Py)",
+    version="0.1.0.0",
+    measurement_type="DAQmx",
+    product_type="DAQ",
+    ui_file_path=os.path.join(os.path.dirname(os.path.abspath(__file__)), "DAQmxMeasurement.measui"),
+    ui_file_type=nims.UIFileType.MeasurementUI,
+)
+
+service_info = nims.ServiceInfo(
+    service_class="DAQmx_Measurement_Python",
+    service_id="{D8B7FF34-3532-4604-9EEE-B5AFC43B7909}",
+    description_url="https://www.ni.com/measurementservices/daqmxmeasurement.html",
+)
+
+daqmx_measurement_service = nims.MeasurementService(measurement_info, service_info)
+
+
+@daqmx_measurement_service.register_measurement
+@daqmx_measurement_service.configuration("Resource name", nims.DataType.String, "Dev1")
+@daqmx_measurement_service.output("Measurements", nims.DataType.DoubleArray1D)
+def measure(resource_name):
+    """User Measurement API. Returns Voltage Measurement as the only output.
+
+    Returns
+    -------
+        Tuple of Output Variables, in the order configured in the metadata.py
+
+    """
+    # User Logic :
+    print("Executing DAQmxMeasurement(Py)")
+
+    def cancel_callback():
+        print("Canceling DAQmxMeasurement(Py)")
+        if task is not None:
+            task.control(nidaqmx.constants.TaskMode.TASK_ABORT)
+            
+    daqmx_measurement_service.context.add_cancel_callback(cancel_callback)
+    time_remaining = daqmx_measurement_service.context.time_remaining()
+
+    timeout = hightime.timedelta(seconds=(min(time_remaining, 10.0)))
+    measured_values = []
+    with nidaqmx.Task() as task:
+        task.ai_channels.add_ai_voltage_chan("Dev1/ai0")
+        measured_values = task.read(number_of_samples_per_channel=10, timeout=timeout)
+        task = None  # Don't abort after this point
+
+    for i, measured_value in measured_values:
+        print("Measured Value:", measured_value)
+        print("---------------------------------")
+    return measured_values
+
+@click.command
+@click.option(
+    "-v", "--verbose", count=True, help="Enable verbose logging. Repeat to increase verbosity."
+)
+def main(verbose: int):
+    """Host the DAQmx Measurement (Screen UI) service."""
+    if verbose > 1:
+        level = logging.DEBUG
+    elif verbose == 1:
+        level = logging.INFO
+    else:
+        level = logging.WARNING
+    logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s", level=level)
+
+    daqmx_measurement_service.host_service()
+    input("Press enter to close the measurement service.\n")
+    daqmx_measurement_service.close_service()
+
+
+if __name__ == "__main__":
+    sys.exit(main())
