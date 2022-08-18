20fffb86cd50246020054d13499e636afdb5b2b0
Brad Keryan <brad.keryan@ni.com>
2022-08-18 11:59:26 -0500

    examples: Update start.bat

--- examples/sample_measurement/start.bat
+++ examples/sample_measurement/start.bat
@@ -1,3 +1,3 @@
-REM Contains script to start the measurement. User can modify the script based on their custom python setup.
+REM The discovery service uses this script to start the measurement service. You can customize this script for your Python setup.
 
-.venv\Scripts\python.exe .\measurement.py
+.venv\Scripts\python.exe measurement.py
