7388285aeae5896f07edd383866e4de13928229a
Wesley Tang <wesley.tang@ni.com>
2022-08-19 14:50:25 -0500

    Renamed everything to specify that this is an analog input measurement.

--- examples/daqmx_measurement_measurementUI/measurement.py
+++ examples/daqmx_measurement_measurementUI/measurement.py
@@ -15,30 +15,30 @@ import ni_measurement_service as nims
 
 
 measurement_info = nims.MeasurementInfo(
-    display_name="DAQmxMeasurement(Py)",
+    display_name="DAQmx Analog Input Measurement(Py)",
     version="0.1.0.0",
-    measurement_type="DAQmx",
+    measurement_type="DAQmx Analog Input",
     product_type="DAQ",
     ui_file_path=os.path.join(
-        os.path.dirname(os.path.abspath(__file__)), "DAQmxMeasurement.measui"
+        os.path.dirname(os.path.abspath(__file__)), "DAQmxAnalogInputMeasurement.measui"
     ),
     ui_file_type=nims.UIFileType.MeasurementUI,
 )
 
 service_info = nims.ServiceInfo(
-    service_class="DAQmx_Measurement_Python",
+    service_class="DAQmx_Analog_Input_Measurement_Python",
     service_id="{D8B7FF34-3532-4604-9EEE-B5AFC43B7909}",
     description_url="https://www.ni.com/measurementservices/daqmxmeasurement.html",
 )
 
-daqmx_measurement_service = nims.MeasurementService(measurement_info, service_info)
+daqmx_analog_input_measurement_service = nims.MeasurementService(measurement_info, service_info)
 
 
-@daqmx_measurement_service.register_measurement
-@daqmx_measurement_service.configuration("Physical Channel", nims.DataType.String, "Dev1/ai0")
-@daqmx_measurement_service.configuration("Sample Rate", nims.DataType.Double, 1000.0)
-@daqmx_measurement_service.configuration("Number of Samples", nims.DataType.UInt64, 100)
-@daqmx_measurement_service.output("Voltage Measurements(V)", nims.DataType.DoubleArray1D)
+@daqmx_analog_input_measurement_service.register_measurement
+@daqmx_analog_input_measurement_service.configuration("Physical Channel", nims.DataType.String, "Dev1/ai0")
+@daqmx_analog_input_measurement_service.configuration("Sample Rate", nims.DataType.Double, 1000.0)
+@daqmx_analog_input_measurement_service.configuration("Number of Samples", nims.DataType.UInt64, 100)
+@daqmx_analog_input_measurement_service.output("Voltage Measurements(V)", nims.DataType.DoubleArray1D)
 def measure(physical_channel, sample_rate, number_of_samples):
     """User Measurement API. Returns Voltage Measurement as the only output.
 
@@ -48,15 +48,15 @@ def measure(physical_channel, sample_rate, number_of_samples):
 
     """
     # User Logic :
-    print("Executing DAQmxMeasurement(Py)")
+    print("Executing DAQmx Analog Input Measurement(Py)")
 
     def cancel_callback():
-        print("Canceling DAQmxMeasurement(Py)")
+        print("Canceling DAQmx Analog Input Measurement(Py)")
         if task is not None:
             task.control(nidaqmx.constants.TaskMode.TASK_ABORT)
 
-    daqmx_measurement_service.context.add_cancel_callback(cancel_callback)
-    time_remaining = daqmx_measurement_service.context.time_remaining()
+    daqmx_analog_input_measurement_service.context.add_cancel_callback(cancel_callback)
+    time_remaining = daqmx_analog_input_measurement_service.context.time_remaining()
 
     timeout = min(time_remaining, 10.0)
     with nidaqmx.Task() as task:
@@ -80,7 +80,7 @@ def measure(physical_channel, sample_rate, number_of_samples):
     "-v", "--verbose", count=True, help="Enable verbose logging. Repeat to increase verbosity."
 )
 def main(verbose: int):
-    """Host the DAQmx Measurement (Screen UI) service."""
+    """Host the DAQmx Analog Input Measurement (Screen UI) service."""
     if verbose > 1:
         level = logging.DEBUG
     elif verbose == 1:
@@ -89,9 +89,9 @@ def main(verbose: int):
         level = logging.WARNING
     logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s", level=level)
 
-    daqmx_measurement_service.host_service()
+    daqmx_analog_input_measurement_service.host_service()
     input("Press enter to close the measurement service.\n")
-    daqmx_measurement_service.close_service()
+    daqmx_analog_input_measurement_service.close_service()
 
 
 if __name__ == "__main__":
