158ec18be41599b61702bf4a7b10c4e8f7a23fb0
Joel Dixon <38357562+dixonjoel@users.noreply.github.com>
2022-08-30 09:40:03 -0500

    Use the current working directory as the default output directory (#85)
    * Use the current working directory as the default and not the location of template.py. Add a subfolder for the display name if the output directory is not specified
    
    * Use path and check if directory_out is None. Revert to version 0.1.0
    
    * Remove unneeded import.
    
    * Revert poetry.lock
    
    * Eliminate spaces in file names

--- ni_measurement_generator/ni_measurement_generator/template.py
+++ ni_measurement_generator/ni_measurement_generator/template.py
@@ -55,9 +55,9 @@ def _get_ui_type(ui_file):
         )
 
 
-def _resolve_ui_file(ui_file, display_name):
+def _resolve_ui_file(ui_file, display_name_for_filenames):
     if ui_file is None:
-        return f"{display_name}.measui"
+        return f"{display_name_for_filenames}.measui"
     else:
         return ui_file
 
@@ -113,8 +113,7 @@ def _check_guid(ctx, param, service_id):
 @click.option(
     "-o",
     "--directory-out",
-    default=pathlib.Path(__file__).parent,
-    help="Output directory for measurement files.",
+    help="Output directory for measurement files. Default is the current directory/<display_name>",
 )
 def create_measurement(
     display_name,
@@ -145,8 +144,15 @@ def create_measurement(
     e.g. ADC, LDO.
     """
     service_class = _resolve_service_class(service_class, display_name)
-    ui_file = _resolve_ui_file(ui_file, display_name)
+    display_name_for_filenames = re.sub(r"\s+", "", display_name)
+    ui_file = _resolve_ui_file(ui_file, display_name_for_filenames)
     ui_file_type = _get_ui_type(ui_file)
+    if directory_out is None:
+        directory_out = pathlib.Path.cwd() / display_name_for_filenames
+    else:
+        directory_out = pathlib.Path(directory_out)
+
+    directory_out.mkdir(exist_ok=True, parents=True)
 
     _create_file(
         "measurement.py.mako",
@@ -164,7 +170,7 @@ def create_measurement(
     )
     _create_file(
         "measurement.serviceconfig.mako",
-        f"{display_name}.serviceconfig",
+        f"{display_name_for_filenames}.serviceconfig",
         directory_out,
         display_name=display_name,
         service_class=service_class,
@@ -182,7 +188,7 @@ def create_measurement(
         )
         _create_file(
             "measurement.measproj.mako",
-            f"{display_name}.measproj",
+            f"{display_name_for_filenames}.measproj",
             directory_out,
             ui_file=ui_file,
         )
