75608621e7c1903d6028309ca8f4e6e62f2696fa
Brad Keryan <brad.keryan@ni.com>
2022-11-07 19:28:49 -0600

    examples: Update start.bat to use @echo off and -v

--- examples/daqmx_analog_input_measurement/start.bat
+++ examples/daqmx_analog_input_measurement/start.bat
@@ -1,3 +1,4 @@
+@echo off
 REM The discovery service uses this script to start the measurement service. You can customize this script for your Python setup.
 
-.venv\Scripts\python.exe measurement.py
+.venv\Scripts\python.exe measurement.py -v
