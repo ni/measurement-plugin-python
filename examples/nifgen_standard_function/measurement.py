3aa7f631cd0a6fdb05299aff9eda88dee07c96d9
Brad Keryan <brad.keryan@ni.com>
2022-11-17 12:09:50 -0600

    Add nifgen_standard_function example

---
+++ examples/nifgen_standard_function/measurement.py
@@ -0,0 +1,212 @@
+"""Generate a standard function waveform using an NI-FGEN instrument."""
+
+import contextlib
+import logging
+import pathlib
+import sys
+import time
+from typing import List, NamedTuple, Tuple
+
+import click
+import hightime
+import grpc
+import nifgen
+
+import ni_measurement_service as nims
+
+
+measurement_info = nims.MeasurementInfo(
+    display_name="NI-FGEN Standard Function (Py)",
+    version="0.1.0.0",
+    ui_file_paths=[
+        pathlib.Path(__file__).resolve().parent / "NIFgenStandardFunction.measui"
+    ],
+)
+
+service_info = nims.ServiceInfo(
+    service_class="ni.examples.NIFgenStandardFunction_Python",
+    description_url="https://www.ni.com/measurementservices/nifgenstandardfunction.html",
+)
+
+measurement_service = nims.MeasurementService(measurement_info, service_info)
+
+
+class ServiceOptions(NamedTuple):
+    """Service options specified on the command line."""
+
+    use_grpc_device: bool
+    grpc_device_address: str
+
+
+service_options = ServiceOptions(use_grpc_device=False, grpc_device_address="")
+
+
+WAVEFORM_TYPE_TO_ENUM = {
+    "Sine": nifgen.Waveform.SINE,
+    "Square": nifgen.Waveform.SQUARE,
+    "Triangle": nifgen.Waveform.TRIANGLE,
+    "Ramp Up": nifgen.Waveform.RAMP_UP,
+    "Ramp Down": nifgen.Waveform.RAMP_DOWN,
+    "DC": nifgen.Waveform.DC,
+    "Noise": nifgen.Waveform.NOISE,
+}
+
+
+@measurement_service.register_measurement
+@measurement_service.configuration("pin_name", nims.DataType.Pin, "Pin1")
+@measurement_service.configuration("waveform_type", nims.DataType.String, "Sine")
+@measurement_service.configuration("frequency", nims.DataType.Double, 1.0e6)
+@measurement_service.configuration("amplitude", nims.DataType.Double, 2.0)
+@measurement_service.configuration("duration", nims.DataType.Double, 10.0)
+@measurement_service.configuration("abort_when_done", nims.DataType.Boolean, True)
+def measure(
+    pin_name: str,
+    waveform_type: str,
+    frequency: float,
+    amplitude: float,
+    duration: float,
+    abort_when_done: bool,
+) -> Tuple:
+    """Generate a standard function waveform using an NI-FGEN instrument."""
+    logging.info(
+        "Starting generation: pin_name=%s waveform_type=%s frequency=%g amplitude=%g",
+        pin_name,
+        waveform_type,
+        frequency,
+        amplitude,
+    )
+
+    try:
+        waveform_enum = WAVEFORM_TYPE_TO_ENUM[waveform_type]
+    except KeyError as e:
+        raise grpc.RpcError(
+            grpc.StatusCode.INVALID_ARGUMENT,
+            f'Unsupported waveform type "{waveform_type}"',
+        ) from e
+
+    pending_cancellation = False
+
+    def cancel_callback():
+        logging.info("Canceling generation")
+        nonlocal pending_cancellation
+        pending_cancellation = True
+
+    measurement_service.context.add_cancel_callback(cancel_callback)
+
+    session_management_client = nims.session_management.Client(
+        grpc_channel=measurement_service.get_channel(
+            provided_interface=nims.session_management.GRPC_SERVICE_INTERFACE_NAME,
+            service_class=nims.session_management.GRPC_SERVICE_CLASS,
+        )
+    )
+
+    with contextlib.ExitStack() as stack:
+        reservation = stack.enter_context(
+            session_management_client.reserve_sessions(
+                context=measurement_service.context.pin_map_context,
+                pin_names=[pin_name],
+                instrument_type_id="niFGen",
+                timeout=-1,
+            )
+        )
+
+        sessions = [
+            stack.enter_context(_create_session(session_info.resource_name))
+            for session_info in reservation.session_info
+        ]
+
+        for session, session_info in zip(sessions, reservation.session_info):
+            # Output mode must be the same for all channels in the session.
+            session.output_mode = nifgen.OutputMode.FUNC
+
+            channels = session.channels[session_info.channel_list]
+            channels.configure_standard_waveform(waveform_enum, amplitude, frequency)
+
+            session.initiate()
+
+        stop_time = time.time() + duration
+        while True:
+            if time.time() >= stop_time:
+                break
+            if measurement_service.context.time_remaining <= 0:
+                measurement_service.context.abort(
+                    grpc.StatusCode.DEADLINE_EXCEEDED, "Deadline exceeded."
+                )
+            if pending_cancellation:
+                measurement_service.context.abort(
+                    grpc.StatusCode.CANCELLED, "Client requested cancellation."
+                )
+            if any((session.is_done() for session in sessions)):
+                break
+            sessions[0].wait_until_done(hightime.timedelta(milliseconds=100))
+
+        if abort_when_done:
+            logging.info("Aborting generation")
+            for session in sessions:
+                session.abort()
+
+    return ()
+
+
+def _create_session(resource_name: str) -> nifgen.Session:
+    session_kwargs = {}
+    if service_options.use_grpc_device:
+        session_grpc_address = service_options.grpc_device_address
+
+        if not session_grpc_address:
+            session_grpc_channel = measurement_service.get_channel(
+                provided_interface=nifgen.GRPC_SERVICE_INTERFACE_NAME,
+                service_class="ni.measurementlink.v1.grpcdeviceserver",
+            )
+        else:
+            session_grpc_channel = measurement_service.channel_pool.get_channel(
+                target=session_grpc_address
+            )
+        session_kwargs["_grpc_options"] = nifgen.GrpcSessionOptions(
+            session_grpc_channel,
+            session_name=resource_name,
+            initialization_behavior=nifgen.SessionInitializationBehavior.AUTO,
+        )
+
+    return nifgen.Session(resource_name=resource_name, **session_kwargs)
+
+
+@click.command
+@click.option(
+    "-v",
+    "--verbose",
+    count=True,
+    help="Enable verbose logging. Repeat to increase verbosity.",
+)
+@click.option(
+    "--use-grpc-device",
+    default=False,
+    is_flag=True,
+    help="Use the NI gRPC Device Server.",
+)
+@click.option(
+    "--grpc-device-address",
+    default="",
+    help="NI gRPC Device Server address (e.g. localhost:31763). If unspecified, use the discovery service to resolve the address.",
+)
+def main(verbose: int, use_grpc_device: bool, grpc_device_address: str):
+    """Host the DC Measurement (Screen UI) service."""
+    if verbose > 1:
+        level = logging.DEBUG
+    elif verbose == 1:
+        level = logging.INFO
+    else:
+        level = logging.WARNING
+    logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s", level=level)
+
+    global service_options
+    service_options = ServiceOptions(
+        use_grpc_device=use_grpc_device, grpc_device_address=grpc_device_address
+    )
+
+    with measurement_service.host_service():
+        input("Press enter to close the measurement service.\n")
+
+
+if __name__ == "__main__":
+    sys.exit(main())
