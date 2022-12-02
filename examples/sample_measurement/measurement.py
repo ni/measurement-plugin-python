2a7d17e9ee9e297cc4d7e3a8e80b7f8a8e723271
Vasantha_Kumar_J <vasanthakumar2598@gmail.com>
2022-12-02 17:22:25 +0530

    Fix lint errors

--- examples/sample_measurement/measurement.py
+++ examples/sample_measurement/measurement.py
@@ -36,7 +36,9 @@ sample_measurement_service = nims.MeasurementService(measurement_info, service_i
 )
 @sample_measurement_service.configuration("Bool In", nims.DataType.Boolean, False)
 @sample_measurement_service.configuration("String In", nims.DataType.String, "sample string")
-@sample_measurement_service.configuration("String Array In", nims.DataType.StringArray1D, ["String1", "String2"])
+@sample_measurement_service.configuration(
+    "String Array In", nims.DataType.StringArray1D, ["String1", "String2"]
+)
 @sample_measurement_service.output("Float out", nims.DataType.Float)
 @sample_measurement_service.output("Double Array out", nims.DataType.DoubleArray1D)
 @sample_measurement_service.output("Bool out", nims.DataType.Boolean)
@@ -58,7 +60,7 @@ def measure(float_input, double_array_input, bool_input, string_input, string_ar
     string_output = string_input
     logging.info("Completed measurement")
 
-    return (float_output, float_array_output, bool_output, string_output,string_array_in)
+    return (float_output, float_array_output, bool_output, string_output, string_array_in)
 
 
 @click.command
