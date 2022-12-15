7f2f7b22ea1b819af20ab019723910c0e6fca61d
Brad Keryan <brad.keryan@ni.com>
2022-12-15 16:13:45 -0600

    install_examples.py: Run Black

--- scripts/install_examples.py
+++ scripts/install_examples.py
@@ -7,7 +7,13 @@ import subprocess
 import os
 
 EXAMPLES_PATH = pathlib.Path(__file__).parent.parent / "examples"
-SERVICES_PATH = pathlib.Path(os.environ["ProgramData"]) / "National Instruments" / "MeasurementLink" / "Services"
+SERVICES_PATH = (
+    pathlib.Path(os.environ["ProgramData"])
+    / "National Instruments"
+    / "MeasurementLink"
+    / "Services"
+)
+
 
 def main():
     """Install all example services."""
@@ -30,23 +36,24 @@ def main():
 
         print(f"Installing dependencies")
         subprocess.run(["poetry", "-v", "install"], check=True, cwd=example_path)
-        
+
         install_path = SERVICES_PATH / example_path.name
         if install_path.is_dir():
             print(f"Deleting example from {install_path}")
             shutil.rmtree(install_path)
-        
+
         print(f"Installing example into {install_path}")
         shutil.copytree(example_path, install_path)
 
         print("")
+
 
 def _deactivate_venv():
     """Deactivate the current venv.
 
     This is a workaround for https://github.com/python-poetry/poetry/issues/4055
     Option to force Poetry to create a virtual environment, even if a virtual env is active
-     
+
     """
     if "VIRTUAL_ENV" in os.environ:
         for var in os.environ.keys():
@@ -55,6 +62,7 @@ def _deactivate_venv():
                 os.environ[original_var] = os.environ[var]
                 del os.environ[var]
         del os.environ["VIRTUAL_ENV"]
+
 
 if __name__ == "__main__":
     main()
