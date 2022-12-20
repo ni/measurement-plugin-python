0a34ef79d75cf773461a7767bd274a847e851f94
Brad Keryan <brad.keryan@ni.com>
2022-12-20 13:51:38 -0600

    session_management.py: Fix import order

--- ni_measurement_service/session_management.py
+++ ni_measurement_service/session_management.py
@@ -4,12 +4,12 @@ from typing import Iterable, List, NamedTuple, Optional
 
 import grpc
 
+from ni_measurement_service._internal.stubs import session_pb2
 from ni_measurement_service._internal.stubs.ni.measurementlink import pin_map_context_pb2
 from ni_measurement_service._internal.stubs.ni.measurementlink.sessionmanagement.v1 import (
     session_management_service_pb2,
     session_management_service_pb2_grpc,
 )
-from ni_measurement_service._internal.stubs import session_pb2
 
 GRPC_SERVICE_INTERFACE_NAME = "ni.measurementlink.sessionmanagement.v1.SessionManagementService"
 GRPC_SERVICE_CLASS = "ni.measurementlink.sessionmanagement.v1.SessionManagementService"
