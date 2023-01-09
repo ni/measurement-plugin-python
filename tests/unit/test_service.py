59131b29df0f3c0b8dcbf74501990abed6c68c6f
Joel Dixon <38357562+dixonjoel@users.noreply.github.com>
2023-01-09 11:06:10 -0600

    Path type specialization support (#185)
    * Path type specialization support
    
    * Add path parameters to configuration test
    
    * Add separate path configuration tests
    
    * Fix lint errors

--- tests/unit/test_service.py
+++ tests/unit/test_service.py
@@ -26,8 +26,8 @@ def test___measurement_service___register_measurement_method___method_registered
         ("Int32", DataType.Int32, -8799),
         ("Int64", DataType.Int64, -999),
         ("UInt32", DataType.UInt32, 3994),
-        ("UInt44", DataType.UInt64, 3456),
-        ("UInt44", DataType.UInt64, False),
+        ("UInt64", DataType.UInt64, 3456),
+        ("UInt64", DataType.UInt64, False),
     ],
 )
 def test___measurement_service___add_configuration__configuration_added(
@@ -101,7 +101,63 @@ def test___measurement_service___add_non_pin_configuration__pin_type_annotations
     measurement_service.configuration(display_name, type, default_value)(_fake_measurement_function)
 
     assert not all(
-        param.annotations.get("ni/type_specializaation") == TypeSpecialization.Pin.value
+        param.annotations.get("ni/type_specialization") == TypeSpecialization.Pin.value
+        for param in measurement_service.configuration_parameter_list
+    )
+
+
+@pytest.mark.parametrize(
+    "display_name,type,default_value",
+    [
+        ("PathConfiguration", DataType.Path, "path1"),
+        ("PathArrayConfiguration", DataType.PathArray1D, ["path1", "path2"]),
+    ],
+)
+def test___measurement_service___add_path_configuration__path_configuration_added(
+    display_name, type, default_value
+):
+    """Test to validate the configuration decorator."""
+    measurement_service = MeasurementService(None, None)
+
+    measurement_service.configuration(display_name, type, default_value)(_fake_measurement_function)
+
+    assert any(
+        param.display_name == display_name
+        and param.type == type.value[0]
+        and param.repeated == type.value[1]
+        and param.default_value == default_value
+        and param.annotations
+        == {
+            "ni/type_specialization": TypeSpecialization.Path.value,
+        }
+        for param in measurement_service.configuration_parameter_list
+    )
+
+
+@pytest.mark.parametrize(
+    "display_name,type,default_value",
+    [
+        ("BoolConfiguration", DataType.Boolean, True),
+        ("StringConfiguration", DataType.String, "DefaultString"),
+        ("DoubleConfiguration", DataType.Double, 0.899),
+        ("Float", DataType.Float, 0.100),
+        ("Double1DArray", DataType.DoubleArray1D, [1.009, -1.0009]),
+        ("Int32", DataType.Int32, -8799),
+        ("Int64", DataType.Int64, -999),
+        ("UInt32", DataType.UInt32, 3994),
+        ("UInt64", DataType.UInt64, 3456),
+    ],
+)
+def test___measurement_service___add_non_path_configuration__path_type_annotations_not_added(
+    display_name, type, default_value
+):
+    """Test to validate the configuration decorator."""
+    measurement_service = MeasurementService(None, None)
+
+    measurement_service.configuration(display_name, type, default_value)(_fake_measurement_function)
+
+    assert not all(
+        param.annotations.get("ni/type_specialization") == TypeSpecialization.Path.value
         for param in measurement_service.configuration_parameter_list
     )
 
@@ -120,6 +176,8 @@ def test___measurement_service___add_non_pin_configuration__pin_type_annotations
         ("UInt44", DataType.UInt64, ""),
         ("Pin", DataType.Pin, 1.0),
         ("Pin1DArray", DataType.PinArray1D, [1.009, -1.0009]),
+        ("Path", DataType.Path, 1.0),
+        ("Path1DArray", DataType.PathArray1D, [1.009, -1.0009]),
     ],
 )
 def test___measurement_service___add_configuration_with_mismatch_default_value__raises_type_error(
