aad231430c8ac9b36fb1bf055a19aaa8c7935869
Chris Delpire <chris.delpire@ni.com>
2023-03-13 16:15:51 -0500

    Adding autoapi/index to index.rst so api shows up in sidebar.
    Signed-off-by: Chris Delpire <chris.delpire@ni.com>

--- _docs_source/conf.py
+++ _docs_source/conf.py
@@ -62,5 +62,9 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 #
 html_theme = "sphinx_rtd_theme"
 
+html_theme_options = {
+    'navigation_depth': -1,
+}
+
 # Napoleon settings
 napoleon_numpy_docstring = False
