f1749fbb105a23c8020904b68aa2a8ce43f09b08
Brad Keryan <brad.keryan@ni.com>
2023-04-28 19:26:13 -0500

    service: Fix import order
    Signed-off-by: Brad Keryan <brad.keryan@ni.com>

--- ni_measurementlink_service/__init__.py
+++ ni_measurementlink_service/__init__.py
@@ -1,12 +1,11 @@
 """MeasurementLink Support for Python."""
 import logging
 
-from .measurement.info import DataType
-from .measurement.info import MeasurementInfo
-from .measurement.info import ServiceInfo
-from .measurement.service import MeasurementService
-
 from ni_measurementlink_service import session_management
+from ni_measurementlink_service.measurement.info import DataType
+from ni_measurementlink_service.measurement.info import MeasurementInfo
+from ni_measurementlink_service.measurement.info import ServiceInfo
+from ni_measurementlink_service.measurement.service import MeasurementService
 
 _logger = logging.getLogger(__name__)
 _logger.addHandler(logging.NullHandler())
