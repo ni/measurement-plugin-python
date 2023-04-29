697e9e2c9f354b59bf62edacec04305adead2be8
Brad Keryan <brad.keryan@ni.com>
2023-04-28 19:23:05 -0500

    Remove noqa comments
    Signed-off-by: Brad Keryan <brad.keryan@ni.com>

--- ni_measurementlink_service/__init__.py
+++ ni_measurementlink_service/__init__.py
@@ -1,12 +1,12 @@
 """MeasurementLink Support for Python."""
 import logging
 
-from .measurement.info import DataType  # noqa F401, declaring API
-from .measurement.info import MeasurementInfo  # noqa F401, declaring API
-from .measurement.info import ServiceInfo  # noqa F401, declaring API
-from .measurement.service import MeasurementService  # noqa F401, declaring API
+from .measurement.info import DataType
+from .measurement.info import MeasurementInfo
+from .measurement.info import ServiceInfo
+from .measurement.service import MeasurementService
 
-from ni_measurementlink_service import session_management  # noqa F401, declaring API
+from ni_measurementlink_service import session_management
 
 _logger = logging.getLogger(__name__)
 _logger.addHandler(logging.NullHandler())
