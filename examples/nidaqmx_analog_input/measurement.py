84985cfacd1923327ad03b9d9e69fd5873742124
Sam Chrisvin <104556074+samchris007@users.noreply.github.com>
2023-05-08 16:58:08 +0530

    Draft: Update nidaqmx analog input example with latest nidaqmx-python changes

--- examples/nidaqmx_analog_input/measurement.py
+++ examples/nidaqmx_analog_input/measurement.py
@@ -1,13 +1,21 @@
 """Perform a finite analog input measurement with NI-DAQmx."""
 
+import contextlib
 import logging
 import pathlib
+import sys
 
 import click
+import grpc
 import nidaqmx
-from _helpers import configure_logging, verbosity_option
 
 import ni_measurementlink_service as nims
+from nidaqmx.grpc_session_options import (
+    GrpcSessionOptions,
+    GRPC_SERVICE_INTERFACE_NAME,
+    SessionInitializationBehavior,
+)
+from _helpers import ServiceOptions
 
 service_directory = pathlib.Path(__file__).resolve().parent
 measurement_service = nims.MeasurementService(
@@ -15,10 +23,16 @@ measurement_service = nims.MeasurementService(
     version="0.1.0.0",
     ui_file_paths=[service_directory / "NIDAQmxAnalogInput.measui"],
 )
+service_options = ServiceOptions()
 
 
 @measurement_service.register_measurement
-@measurement_service.configuration("physical_channel", nims.DataType.String, "Dev1/ai0")
+@measurement_service.configuration(
+    "pin_name",
+    nims.DataType.Pin,
+    "Pin1",
+    instrument_type=nims.session_management.INSTRUMENT_TYPE_NI_DAQMX,
+)
 @measurement_service.configuration("sample_rate", nims.DataType.Double, 1000.0)
 @measurement_service.configuration("number_of_samples", nims.DataType.UInt64, 100)
 @measurement_service.output("acquired_samples", nims.DataType.DoubleArray1D)
@@ -30,29 +44,91 @@ def measure(physical_channel, sample_rate, number_of_samples):
         sample_rate,
         number_of_samples,
     )
+    session_management_client = nims.session_management.Client(
+        grpc_channel=measurement_service.get_channel(
+            provided_interface=nims.session_management.GRPC_SERVICE_INTERFACE_NAME,
+            service_class=nims.session_management.GRPC_SERVICE_CLASS,
+        )
+    )
+    with contextlib.ExitStack() as stack:
+        reservation = stack.enter_context(
+            session_management_client.reserve_sessions(
+                context=measurement_service.context.pin_map_context,
+                pin_or_relay_names=[physical_channel],
+                instrument_type_id=nims.session_management.INSTRUMENT_TYPE_NI_DAQMX,
+                # If another measurement is using the session, wait for it to complete.
+                # Specify a timeout to aid in debugging missed unreserve calls.
+                # Long measurements may require a longer timeout.
+                timeout=60,
+            )
+        )
+        if len(reservation.session_info) != 1:
+            measurement_service.context.abort(
+                grpc.StatusCode.INVALID_ARGUMENT,
+                f"Unsupported number of sessions: {len(reservation.session_info)}",
+            )
+        session_info = reservation.session_info[0]
+        task = stack.enter_context(_create_nidaqmx_task(session_info))
+        channel_mappings = session_info.channel_mappings
+        pending_cancellation = False
 
-    def cancel_callback():
-        logging.info("Canceling measurement")
-        task_to_abort = task
-        if task_to_abort is not None:
-            task_to_abort.control(nidaqmx.constants.TaskMode.TASK_ABORT)
+        def cancel_callback():
+            logging.info("Canceling measurement")
+            session_to_abort = task
+            task_to_abort = task
+            if task_to_abort is not None:
+                nonlocal pending_cancellation
+                pending_cancellation = True
+                session_to_abort.abort()
 
-    measurement_service.context.add_cancel_callback(cancel_callback)
-    time_remaining = measurement_service.context.time_remaining
+        measurement_service.context.add_cancel_callback(cancel_callback)
+        time_remaining = measurement_service.context.time_remaining
 
-    timeout = min(time_remaining, 10.0)
-    with nidaqmx.Task() as task:
-        task.ai_channels.add_ai_voltage_chan(physical_channel)
+        timeout = min(time_remaining, 10.0)
+        channel = next(
+            channel_mapping.channel
+            for channel_mapping in channel_mappings
+            if channel_mapping.pin_or_relay_name == physical_channel
+        )
+        task.ai_channels.add_ai_voltage_chan(channel, min_val=0, max_val=5)
         task.timing.cfg_samp_clk_timing(
             rate=sample_rate,
             samps_per_chan=number_of_samples,
         )
-        voltage_values = task.read(number_of_samples_per_channel=number_of_samples, timeout=timeout)
+        voltage_values = task.read(
+            number_of_samples_per_channel=number_of_samples, timeout=timeout
+        )
         task = None  # Don't abort after this point
 
     _log_measured_values(voltage_values)
     logging.info("Completed measurement")
     return (voltage_values,)
+
+
+def _create_nidaqmx_task(
+    session_info: nims.session_management.SessionInformation,
+) -> nidaqmx.Task:
+    if service_options.use_grpc_device:
+        session_grpc_address = service_options.grpc_device_address
+
+        if not session_grpc_address:
+            session_grpc_channel = measurement_service.get_channel(
+                provided_interface=GRPC_SERVICE_INTERFACE_NAME,
+                service_class="ni.measurementlink.v1.grpcdeviceserver",
+            )
+        else:
+            session_grpc_channel = measurement_service.channel_pool.get_channel(
+                target=session_grpc_address
+            )
+        grpc_session_options = GrpcSessionOptions(
+            session_grpc_channel,
+            session_name=session_info.session_name,
+            initialization_behavior=SessionInitializationBehavior.AUTO,
+        )
+
+    return nidaqmx.Task(
+        new_task_name=session_info.resource_name, grpc_options=grpc_session_options
+    )
 
 
 def _log_measured_values(samples, max_samples_to_display=5):
@@ -68,14 +144,39 @@ def _log_measured_values(samples, max_samples_to_display=5):
 
 
 @click.command
-@verbosity_option
-def main(verbosity: int) -> None:
+@click.option(
+    "-v",
+    "--verbose",
+    count=True,
+    help="Enable verbose logging. Repeat to increase verbosity.",
+)
+@click.option(
+    "--use-grpc-device/--no-use-grpc-device",
+    default=True,
+    is_flag=True,
+    help="Use the NI gRPC Device Server.",
+)
+@click.option(
+    "--grpc-device-address",
+    default="",
+    help="NI gRPC Device Server address (e.g. localhost:31763). If unspecified, use the discovery service to resolve the address.",
+)
+def main(verbose: int, use_grpc_device: bool, grpc_device_address: str):
     """Perform a finite analog input measurement with NI-DAQmx."""
-    configure_logging(verbosity)
-
+    if verbose > 1:
+        level = logging.DEBUG
+    elif verbose == 1:
+        level = logging.INFO
+    else:
+        level = logging.WARNING
+    logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s", level=level)
+    global service_options
+    service_options = ServiceOptions(
+        use_grpc_device=use_grpc_device, grpc_device_address=grpc_device_address
+    )
     with measurement_service.host_service():
         input("Press enter to close the measurement service.\n")
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
---
+++ examples/nidaqmx_analog_input/teststand_fixture.py
@@ -0,0 +1,93 @@
+"""Functions to set up and tear down sessions of NI-Scope devices in NI TestStand."""
+from typing import Any
+
+import nidaqmx
+from _helpers import GrpcChannelPoolHelper, PinMapClient, TestStandSupport
+from nidaqmx.grpc_session_options import GrpcSessionOptions, GRPC_SERVICE_INTERFACE_NAME, SessionInitializationBehavior
+import ni_measurementlink_service as nims
+
+
+def update_pin_map(pin_map_path: str, sequence_context: Any) -> str:
+    """Update registered pin map contents.
+
+    Create and register a pin map if a pin map resource for the specified pin map id is not found.
+
+    Args:
+        pin_map_path:
+            An absolute or relative path to the pin map file.
+        sequence_context:
+            The SequenceContext COM object from the TestStand sequence execution.
+            (Dynamically typed.)
+    """
+    teststand_support = TestStandSupport(sequence_context)
+    pin_map_abs_path = teststand_support.resolve_file_path(pin_map_path)
+
+    with GrpcChannelPoolHelper() as grpc_channel_pool:
+        pin_map_client = PinMapClient(grpc_channel=grpc_channel_pool.pin_map_channel)
+        pin_map_id = pin_map_client.update_pin_map(pin_map_abs_path)
+
+    teststand_support.set_active_pin_map_id(pin_map_id)
+    return pin_map_id
+
+
+def create_nidaqmx_tasks(sequence_context: Any) -> None:
+    """Create and register all NI-DAQmx tasks.
+
+    Args:
+        sequence_context:
+            The SequenceContext COM object from the TestStand sequence execution.
+            (Dynamically typed.)
+    """
+    with GrpcChannelPoolHelper() as grpc_channel_pool:
+        session_management_client = nims.session_management.Client(
+            grpc_channel=grpc_channel_pool.session_management_channel
+        )
+
+        teststand_support = TestStandSupport(sequence_context)
+        pin_map_id = teststand_support.get_active_pin_map_id()
+
+        pin_map_context = nims.session_management.PinMapContext(pin_map_id=pin_map_id, sites=None)
+        with session_management_client.reserve_sessions(
+            context=pin_map_context,
+            instrument_type_id=nims.session_management.INSTRUMENT_TYPE_NI_DAQMX,
+            # This code module sets up the sessions, so error immediately if they are in use.
+            timeout=0,
+        ) as reservation:
+            for session_info in reservation.session_info:
+                grpc_options = GrpcSessionOptions(
+                    grpc_channel_pool.get_grpc_device_channel(GRPC_SERVICE_INTERFACE_NAME),
+                    session_name=session_info.session_name,
+                    initialization_behavior=SessionInitializationBehavior.INITIALIZE_SERVER_SESSION,
+                )
+
+                # Leave session open
+                nidaqmx.Task(new_task_name=session_info.resource_name, grpc_options=grpc_options)
+
+            session_management_client.register_sessions(reservation.session_info)
+
+
+def destroy_nidaqmx_tasks() -> None:
+    """Destroy and unregister all NI-DAQmx tasks."""
+    with GrpcChannelPoolHelper() as grpc_channel_pool:
+        session_management_client = nims.session_management.Client(
+            grpc_channel=grpc_channel_pool.session_management_channel
+        )
+
+        with session_management_client.reserve_all_registered_sessions(
+            instrument_type_id=nims.session_management.INSTRUMENT_TYPE_NI_DAQMX,
+            # This code module sets up the sessions, so error immediately if they are in use.
+            timeout=0,
+        ) as reservation:
+            session_management_client.unregister_sessions(reservation.session_info)
+
+            for session_info in reservation.session_info:
+                grpc_options = GrpcSessionOptions(
+                    grpc_channel_pool.get_grpc_device_channel(GRPC_SERVICE_INTERFACE_NAME),
+                    session_name=session_info.session_name,
+                    initialization_behavior=SessionInitializationBehavior.ATTACH_TO_SERVER_SESSION,
+                )
+
+                task = nidaqmx.Task(
+                    new_task_name=session_info.resource_name, grpc_options=grpc_options
+                )
+                task.close()
