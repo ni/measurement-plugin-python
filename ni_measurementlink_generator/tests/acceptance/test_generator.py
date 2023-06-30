d3ae482064121f7023677b1b39e4cce8a704fed7
Gonzalo Pacheco <gonzalo.pacheco@ni.com>
2023-06-30 10:35:03 -0500

    Revert change to avoid duplicated files, no need to make it more complex.

--- ni_measurementlink_generator/tests/acceptance/test_generator.py
+++ ni_measurementlink_generator/tests/acceptance/test_generator.py
@@ -29,16 +29,11 @@ def test___command_line_args___create_measurement___render_without_exception(
 
     golden_path = test_assets_directory / "example_renders" / "measurement"
 
-    filenames = [
-        "../measurement.py",
-        "SampleMeasurement.serviceconfig",
-        "../start.bat",
-        "../_helpers.py",
-    ]
+    filenames = ["measurement.py", "SampleMeasurement.serviceconfig", "start.bat", "_helpers.py"]
     for filename in filenames:
         _assert_equal(
             golden_path / filename,
-            temp_directory / filename.replace("../", ""),
+            temp_directory / filename,
         )
 
 
@@ -76,16 +71,11 @@ def test___command_line_args___create_measurement_with_annotations___render_with
 
     golden_path = test_assets_directory / "example_renders" / "measurement_with_annotations"
 
-    filenames = [
-        "../measurement.py",
-        "SampleMeasurement.serviceconfig",
-        "../start.bat",
-        "../_helpers.py",
-    ]
+    filenames = ["measurement.py", "SampleMeasurement.serviceconfig", "start.bat", "_helpers.py"]
     for filename in filenames:
         _assert_equal(
             golden_path / filename,
-            temp_directory / filename.replace("../", ""),
+            temp_directory / filename,
         )
 
 
