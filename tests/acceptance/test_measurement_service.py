fde793cc6bbb04f8d3aad333537fdb2e0719d83c
Vikram Avudaiappan <vikram.avudaiappan@ni.com>
2023-07-19 11:50:11 +0530

    test: Run black

--- tests/acceptance/test_measurement_service.py
+++ tests/acceptance/test_measurement_service.py
@@ -8,7 +8,7 @@ from typing import Generator, List, Union
 
 import pytest
 from google.protobuf import any_pb2
-    
+
 from ni_measurementlink_service._internal.stubs.ni.measurementlink.measurement.v1 import (
     measurement_service_pb2 as v1_measurement_service_pb2,
     measurement_service_pb2_grpc as v1_measurement_service_pb2_grpc,
