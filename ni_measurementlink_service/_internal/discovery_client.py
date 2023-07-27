dfa6666cc7b39417a5d8f3ecfd2ce6f1b211352d
Vikram Avudaiappan <vikram.avudaiappan@ni.com>
2023-07-27 20:23:06 +0530

    discovery_client: Update exception handling for register service function

--- ni_measurementlink_service/_internal/discovery_client.py
+++ ni_measurementlink_service/_internal/discovery_client.py
@@ -135,6 +135,7 @@ class DiscoveryClient:
             _logger.error(
                 "Unable to register with discovery service. Possible reason: discovery service not running."
             )
+            return False
         except Exception:
             _logger.exception("Error in registering with discovery service.")
             return False
