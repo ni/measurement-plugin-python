0bc7ea24bbbdbcc94c0024fe14dfc54533960ef1
mbattu <mounika.battu@ni.com>
2023-08-09 22:35:58 +0530

    Fix: Delete globaltestingstate.py

--- ni_measurementlink_service/_internal/utilities/globaltestingstate.py
+++
@@ -1,8 +0,0 @@
-"""Contains indicators to identify the Production/Test environment.
-"""
-
-
-class GlobalTestingState:
-    """Indicator for measurement running in testing environment."""
-
-    IsInTestState: bool = False
