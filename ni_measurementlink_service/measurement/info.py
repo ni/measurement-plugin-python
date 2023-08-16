f0638484d64ebcaa0422f83a8e3fe508a705481b
Joel Dixon <joel.dixon@ni.com>
2023-08-16 11:17:03 -0500

    Make enum values simple numbers

--- ni_measurementlink_service/measurement/info.py
+++ ni_measurementlink_service/measurement/info.py
@@ -67,29 +67,29 @@ class TypeSpecialization(enum.Enum):
 class DataType(enum.Enum):
     """Enum that represents the supported data types."""
 
-    Int32 = (0,)
-    Int64 = (1,)
-    UInt32 = (2,)
-    UInt64 = (3,)
-    Float = (4,)
-    Double = (5,)
-    Boolean = (6,)
-    String = (7,)
-    Pin = (8,)
-    Path = (9,)
-    Enum = (10,)
+    Int32 = 0
+    Int64 = 1
+    UInt32 = 2
+    UInt64 = 3
+    Float = 4
+    Double = 5
+    Boolean = 6
+    String = 7
+    Pin = 8
+    Path = 9
+    Enum = 10
 
-    Int32Array1D = (100,)
-    Int64Array1D = (101,)
-    UInt32Array1D = (102,)
-    UInt64Array1D = (103,)
-    FloatArray1D = (104,)
-    DoubleArray1D = (105,)
-    BooleanArray1D = (106,)
-    StringArray1D = (107,)
-    PinArray1D = (108,)
-    PathArray1D = (109,)
-    EnumArray1D = (110,)
+    Int32Array1D = 100
+    Int64Array1D = 101
+    UInt32Array1D = 102
+    UInt64Array1D = 103
+    FloatArray1D = 104
+    DoubleArray1D = 105
+    BooleanArray1D = 106
+    StringArray1D = 107
+    PinArray1D = 108
+    PathArray1D = 109
+    EnumArray1D = 110
 
 
 class DataTypeInfo(NamedTuple):
