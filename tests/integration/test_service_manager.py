ecffed0cbbbb50fca99013a28c08262944938027
mbattu <mounika.battu@ni.com>
2023-08-16 23:45:58 +0530

    Fix: Lint errors

--- tests/integration/test_service_manager.py
+++ tests/integration/test_service_manager.py
@@ -97,7 +97,11 @@ def discovery_service_stub(request) -> FakeDiscoveryServiceStub:
     expect_discovery_service_stub_error = _get_marker_value(
         request, "expect_discovery_service_stub_error"
     )
-    return FakeDiscoveryServiceStubError() if expect_discovery_service_stub_error else FakeDiscoveryServiceStub()
+    return (
+        FakeDiscoveryServiceStubError()
+        if expect_discovery_service_stub_error
+        else FakeDiscoveryServiceStub()
+    )
 
 
 def _validate_if_service_running_by_making_rpc(port_number):
