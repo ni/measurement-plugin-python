4c776bf0ded934c269571e786838a3d47b56f3e3
Joel Dixon <joel.dixon@ni.com>
2023-08-21 13:49:00 -0500

    Fix lint errors

--- examples/game_of_life/measurement.py
+++ examples/game_of_life/measurement.py
@@ -5,6 +5,7 @@ import random
 import time
 
 import click
+
 import ni_measurementlink_service as nims
 from ni_measurementlink_service._internal.stubs.ni.protobuf.types import xydata_pb2
 
@@ -24,22 +25,22 @@ measurement_service = nims.MeasurementService(
 @measurement_service.output("generation", nims.DataType.UInt32)
 def measure(width, height, update_interval):
     """Streaming measurement that returns Conway's Game of Life grid as DoubleXYData."""
-    grid = initialize_grid_with_seeded_data(width, height)
+    grid = _initialize_grid_with_seeded_data(width, height)
     generation = 0
     while True:
-        generation += 1;
+        generation += 1
 
-        xydata = initialize_xydata_and_frame(width, height)
-        rowIndex = 0
+        xydata = _initialize_xydata_and_frame(width, height)
+        row_index = 0
         for row in grid:
-            colIndex = 0
+            col_index = 0
             for cell in row:
                 if cell:
-                    xydata.x_data.append(rowIndex)
-                    xydata.y_data.append(colIndex)
-                colIndex += 1
-            rowIndex += 1
-        grid = update_grid(grid)
+                    xydata.x_data.append(row_index)
+                    xydata.y_data.append(col_index)
+                col_index += 1
+            row_index += 1
+        grid = _update_grid(grid)
         xydata_out = xydata
         generation += 1
         time.sleep(update_interval / 1000)
@@ -47,7 +48,7 @@ def measure(width, height, update_interval):
         yield (xydata_out, generation)
 
 
-def initialize_xydata_and_frame(width, height) -> xydata_pb2.DoubleXYData:
+def _initialize_xydata_and_frame(width, height) -> xydata_pb2.DoubleXYData:
     xydata = xydata_pb2.DoubleXYData()
 
     # Frame To keep the graph stable
@@ -61,13 +62,16 @@ def initialize_xydata_and_frame(width, height) -> xydata_pb2.DoubleXYData:
     xydata.y_data.append(height + 1)
     return xydata
 
-def initialize_grid_with_seeded_data(rows, cols, probability=0.6):
+
+def _initialize_grid_with_seeded_data(rows, cols, probability=0.6):
     return [[random.random() < probability for _ in range(cols)] for _ in range(rows)]
 
-def initialize_grid(rows, cols):
+
+def _initialize_grid(rows, cols):
     return [[False for _ in range(cols)] for _ in range(rows)]
 
-def count_neighbors(grid, row, col):
+
+def _count_neighbors(grid, row, col):
     count = 0
     neighbors = [
         (row - 1, col - 1),
@@ -85,11 +89,11 @@ def count_neighbors(grid, row, col):
     return count
 
 
-def update_grid(grid):
-    new_grid = initialize_grid(len(grid), len(grid[0]))
+def _update_grid(grid):
+    new_grid = _initialize_grid(len(grid), len(grid[0]))
     for row in range(len(grid)):
         for col in range(len(grid[0])):
-            neighbors = count_neighbors(grid, row, col)
+            neighbors = _count_neighbors(grid, row, col)
             if grid[row][col]:
                 new_grid[row][col] = neighbors == 2 or neighbors == 3
             else:
