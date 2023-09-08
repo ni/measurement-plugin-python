4b07aac7adb2090e381105c28a54968f0b2fcb7e
Brad Keryan <brad.keryan@ni.com>
2023-09-08 15:57:13 -0500

    tests: Add logging acceptance tests (#373)
    * service: Rename SessionManagementClient
    
    * tests: Add logging acceptance tests
    
    * tests: Fix name conflict between the `request` fixture and a gRPC request variable

---
+++ tests/acceptance/test_logging.py
@@ -0,0 +1,135 @@
+import logging
+import re
+from typing import Generator
+
+import pytest
+from pytest import FixtureRequest, LogCaptureFixture
+
+from ni_measurementlink_service import session_management
+from ni_measurementlink_service._internal.discovery_client import DiscoveryClient
+from ni_measurementlink_service._internal.stubs.ni.measurementlink.measurement.v2 import (
+    measurement_service_pb2 as v2_measurement_service_pb2,
+    measurement_service_pb2_grpc as v2_measurement_service_pb2_grpc,
+)
+from ni_measurementlink_service.measurement.service import MeasurementService
+from ni_measurementlink_service.session_management import SessionManagementClient
+from tests.acceptance.test_streaming_data_measurement import (
+    _get_configuration_parameters as get_streaming_data_configuration_parameters,
+)
+from tests.utilities import loopback_measurement, streaming_data_measurement
+from tests.utilities.discovery_service_process import DiscoveryServiceProcess
+
+
+def test___discovery_client___call___client_call_logged(
+    caplog: LogCaptureFixture, discovery_client: DiscoveryClient
+) -> None:
+    with caplog.at_level(logging.DEBUG):
+        _ = discovery_client.resolve_service(
+            session_management.GRPC_SERVICE_INTERFACE_NAME, session_management.GRPC_SERVICE_CLASS
+        )
+
+    method_name = "/ni.measurementlink.discovery.v1.DiscoveryService/ResolveService"
+    debug_messages = [r.message for r in caplog.records if r.levelno == logging.DEBUG]
+    assert f"gRPC client call starting: {method_name}" in debug_messages
+    assert f"gRPC client call complete: {method_name}" in debug_messages
+
+
+def test___session_management_client___call___client_call_logged(
+    caplog: LogCaptureFixture, request: FixtureRequest
+) -> None:
+    with caplog.at_level(logging.DEBUG):
+        # HACK: set log level before constructing client
+        session_management_client: SessionManagementClient = request.getfixturevalue(
+            "session_management_client"
+        )
+
+        session_management_client.unregister_sessions(session_info=[])
+
+    method_name = (
+        "/ni.measurementlink.sessionmanagement.v1.SessionManagementService/UnregisterSessions"
+    )
+    debug_messages = [r.message for r in caplog.records if r.levelno == logging.DEBUG]
+    assert f"gRPC client call starting: {method_name}" in debug_messages
+    assert f"gRPC client call complete: {method_name}" in debug_messages
+
+
+@pytest.mark.service_class("ni.tests.LoopbackMeasurement_Python")
+def test___loopback_measurement___get_metadata___server_call_logged(
+    caplog: LogCaptureFixture, request: FixtureRequest
+) -> None:
+    with caplog.at_level(logging.DEBUG):
+        # HACK: set log level before constructing server
+        stub_v2: v2_measurement_service_pb2_grpc.MeasurementServiceStub = request.getfixturevalue(
+            "stub_v2"
+        )
+
+        _ = stub_v2.GetMetadata(v2_measurement_service_pb2.GetMetadataRequest())
+
+    method_name = "/ni.measurementlink.measurement.v2.MeasurementService/GetMetadata"
+    debug_messages = [r.message for r in caplog.records if r.levelno == logging.DEBUG]
+    assert f"gRPC server call starting: {method_name}" in debug_messages
+    assert f"gRPC server call complete: {method_name}" in debug_messages
+    info_messages = [r.message for r in caplog.records if r.levelno == logging.INFO]
+    info_regex = re.compile(rf"gRPC server call {method_name} responded OK in [\d.]+ ms")
+    assert len(list(filter(info_regex.match, info_messages))) == 1
+
+
+@pytest.mark.service_class("ni.tests.StreamingDataMeasurement_Python")
+def test___streaming_data_measurement___measure___server_call_logged(
+    caplog: LogCaptureFixture, request: FixtureRequest
+) -> None:
+    with caplog.at_level(logging.DEBUG):
+        # HACK: set log level before constructing server
+        stub_v2: v2_measurement_service_pb2_grpc.MeasurementServiceStub = request.getfixturevalue(
+            "stub_v2"
+        )
+        num_responses = 10
+
+        measure_request = v2_measurement_service_pb2.MeasureRequest(
+            configuration_parameters=get_streaming_data_configuration_parameters(
+                num_responses=num_responses
+            )
+        )
+        response_iterator = stub_v2.Measure(measure_request)
+        for response in response_iterator:
+            pass
+
+    method_name = "/ni.measurementlink.measurement.v2.MeasurementService/Measure"
+    debug_messages = [r.message for r in caplog.records if r.levelno == logging.DEBUG]
+    assert f"gRPC server call starting: {method_name}" in debug_messages
+    assert f"gRPC server call complete: {method_name}" in debug_messages
+    debug_response_regex = re.compile(rf"gRPC server call streaming response: {method_name}")
+    assert len(list(filter(debug_response_regex.match, debug_messages))) == num_responses
+    info_messages = [r.message for r in caplog.records if r.levelno == logging.INFO]
+    info_regex = re.compile(rf"gRPC server call {method_name} responded OK in [\d.]+ ms")
+    assert len(list(filter(info_regex.match, info_messages))) == 1
+
+
+@pytest.fixture
+def measurement_service(request: FixtureRequest) -> MeasurementService:
+    service_class_marker = request.node.get_closest_marker("service_class")
+    service_class = service_class_marker.args[0]
+    if service_class == "ni.tests.LoopbackMeasurement_Python":
+        return request.getfixturevalue("loopback_measurement_service")
+    elif service_class == "ni.tests.StreamingDataMeasurement_Python":
+        return request.getfixturevalue("streaming_data_measurement_service")
+    else:
+        raise ValueError(f"Unsupported service class: {service_class}")
+
+
+@pytest.fixture
+def loopback_measurement_service(
+    discovery_service_process: DiscoveryServiceProcess,
+) -> Generator[MeasurementService, None, None]:
+    """Test fixture that creates a loopback measurement."""
+    with loopback_measurement.measurement_service.host_service() as service:
+        yield service
+
+
+@pytest.fixture
+def streaming_data_measurement_service(
+    discovery_service_process: DiscoveryServiceProcess,
+) -> Generator[MeasurementService, None, None]:
+    """Test fixture that creates a loopback measurement."""
+    with streaming_data_measurement.measurement_service.host_service() as service:
+        yield service
