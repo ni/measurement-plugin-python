8357c9c17ba6b10b1bca77bc8ffbb7d72b5b9e87
Brad Keryan <brad.keryan@ni.com>
2023-09-08 10:15:00 -0500

    examples: Standardize measurement_service variable name

--- examples/sample_measurement/measurement.py
+++ examples/sample_measurement/measurement.py
@@ -16,7 +16,7 @@ import ni_measurementlink_service as nims
 
 script_or_exe = sys.executable if getattr(sys, "frozen", False) else __file__
 service_directory = pathlib.Path(script_or_exe).resolve().parent
-sample_measurement_service = nims.MeasurementService(
+measurement_service = nims.MeasurementService(
     service_config_path=service_directory / "SampleMeasurement.serviceconfig",
     version="0.1.0.0",
     ui_file_paths=[
@@ -36,34 +36,34 @@ class Color(Enum):
     BLUE = 3
 
 
-@sample_measurement_service.register_measurement
-@sample_measurement_service.configuration("Float In", nims.DataType.Float, 0.06)
-@sample_measurement_service.configuration(
+@measurement_service.register_measurement
+@measurement_service.configuration("Float In", nims.DataType.Float, 0.06)
+@measurement_service.configuration(
     "Double Array In", nims.DataType.DoubleArray1D, [0.1, 0.2, 0.3]
 )
-@sample_measurement_service.configuration("Bool In", nims.DataType.Boolean, False)
-@sample_measurement_service.configuration("String In", nims.DataType.String, "sample string")
-@sample_measurement_service.configuration(
+@measurement_service.configuration("Bool In", nims.DataType.Boolean, False)
+@measurement_service.configuration("String In", nims.DataType.String, "sample string")
+@measurement_service.configuration(
     "Enum In", nims.DataType.Enum, Color.BLUE, enum_type=Color
 )
-@sample_measurement_service.configuration(
+@measurement_service.configuration(
     "Protobuf Enum In",
     nims.DataType.Enum,
     color_pb2.ProtobufColor.BLACK,
     enum_type=color_pb2.ProtobufColor,
 )
-@sample_measurement_service.configuration(
+@measurement_service.configuration(
     "String Array In", nims.DataType.StringArray1D, ["String1", "String2"]
 )
-@sample_measurement_service.output("Float out", nims.DataType.Float)
-@sample_measurement_service.output("Double Array out", nims.DataType.DoubleArray1D)
-@sample_measurement_service.output("Bool out", nims.DataType.Boolean)
-@sample_measurement_service.output("String out", nims.DataType.String)
-@sample_measurement_service.output("Enum out", nims.DataType.Enum, enum_type=Color)
-@sample_measurement_service.output(
+@measurement_service.output("Float out", nims.DataType.Float)
+@measurement_service.output("Double Array out", nims.DataType.DoubleArray1D)
+@measurement_service.output("Bool out", nims.DataType.Boolean)
+@measurement_service.output("String out", nims.DataType.String)
+@measurement_service.output("Enum out", nims.DataType.Enum, enum_type=Color)
+@measurement_service.output(
     "Protobuf Enum out", nims.DataType.Enum, enum_type=color_pb2.ProtobufColor
 )
-@sample_measurement_service.output("String Array out", nims.DataType.StringArray1D)
+@measurement_service.output("String Array out", nims.DataType.StringArray1D)
 def measure(
     float_input,
     double_array_input,
@@ -79,7 +79,7 @@ def measure(
     def cancel_callback():
         logging.info("Canceling measurement")
 
-    sample_measurement_service.context.add_cancel_callback(cancel_callback)
+    measurement_service.context.add_cancel_callback(cancel_callback)
 
     float_output = float_input
     float_array_output = double_array_input
@@ -107,7 +107,7 @@ def main(verbosity: int) -> None:
     """Perform a loopback measurement with various data types."""
     configure_logging(verbosity)
 
-    with sample_measurement_service.host_service():
+    with measurement_service.host_service():
         input("Press enter to close the measurement service.\n")
 
 
