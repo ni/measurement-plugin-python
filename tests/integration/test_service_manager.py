0cebe80478b32e9d55a6f32aea95743c5ec1884b
Joel Dixon <38357562+dixonjoel@users.noreply.github.com>
2023-09-08 14:45:33 -0500

    Only service interfaces listed in .serviceconfig file (#368)
    * Only service interfaces listed in .serviceconfig file
    
    * Fix Ubuntu errors because of incorrect capitalization of a file name
    
    * Fork v1 and v2 only measurements from loopback_measurement
    
    * Use 'in' rather than __contains__
    
    * Use if elseif else for checking interfaces. Add a test for the unknown interface case
    
    * Use correct derived Exception classes

--- tests/integration/test_service_manager.py
+++ tests/integration/test_service_manager.py
@@ -3,6 +3,7 @@ from typing import cast
 
 import grpc
 import pytest
+from grpc import RpcError
 
 from ni_measurementlink_service._internal.discovery_client import DiscoveryClient
 from ni_measurementlink_service._internal.service_manager import GrpcService
@@ -13,7 +14,12 @@ from ni_measurementlink_service._internal.stubs.ni.measurementlink.measurement.v
     measurement_service_pb2,
     measurement_service_pb2_grpc,
 )
-from tests.utilities import loopback_measurement
+from tests.utilities import (
+    loopback_measurement,
+    unknown_interface_measurement,
+    v1_only_measurement,
+    v2_only_measurement,
+)
 from tests.utilities.fake_discovery_service import (
     FakeDiscoveryServiceError,
     FakeDiscoveryServiceStub,
@@ -72,8 +78,50 @@ def test___grpc_service_started___stop_service___service_stopped(grpc_service: G
 
     grpc_service.stop()
 
-    with pytest.raises(Exception):
+    with pytest.raises(RpcError):
         _validate_if_service_running_by_making_rpc(port_number)
+
+
+def test___grpc_service_v2_only___start_service_and_check_v1___raises_error(
+    grpc_service: GrpcService,
+):
+    port_number = grpc_service.start(
+        v2_only_measurement.measurement_service.measurement_info,
+        v2_only_measurement.measurement_service.service_info,
+        v2_only_measurement.measurement_service.configuration_parameter_list,
+        v2_only_measurement.measurement_service.output_parameter_list,
+        v2_only_measurement.measurement_service.measure_function,
+    )
+
+    with pytest.raises(RpcError):
+        _validate_if_service_running_by_making_rpc(port_number)
+
+
+def test___grpc_service_v1_only___start_service_and_check_v1___service_hosted(
+    grpc_service: GrpcService,
+):
+    port_number = grpc_service.start(
+        v1_only_measurement.measurement_service.measurement_info,
+        v1_only_measurement.measurement_service.service_info,
+        v1_only_measurement.measurement_service.configuration_parameter_list,
+        v1_only_measurement.measurement_service.output_parameter_list,
+        v1_only_measurement.measurement_service.measure_function,
+    )
+
+    _validate_if_service_running_by_making_rpc(port_number)
+
+
+def test___grpc_service_unknown_interface___start_service_and_check_v1___raises_error(
+    grpc_service: GrpcService,
+):
+    with pytest.raises(ValueError):
+        grpc_service.start(
+            unknown_interface_measurement.measurement_service.measurement_info,
+            unknown_interface_measurement.measurement_service.service_info,
+            unknown_interface_measurement.measurement_service.configuration_parameter_list,
+            unknown_interface_measurement.measurement_service.output_parameter_list,
+            unknown_interface_measurement.measurement_service.measure_function,
+        )
 
 
 @pytest.fixture
---
+++ tests/utilities/v2_only_measurement/__init__.py
@@ -0,0 +1,23 @@
+"""Contains utility functions to test loopback measurement service. """
+import pathlib
+
+import ni_measurementlink_service as nims
+
+service_directory = pathlib.Path(__file__).resolve().parent
+measurement_service = nims.MeasurementService(
+    service_config_path=service_directory / "v2_only.serviceconfig",
+    version="0.1.0.0",
+    ui_file_paths=[
+        service_directory,
+    ],
+)
+
+
+@measurement_service.register_measurement
+@measurement_service.configuration("Float In", nims.DataType.Float, 0.06)
+@measurement_service.output("Float out", nims.DataType.Float)
+def measure(float_input):
+    """Loopback measurement on the float input."""
+    float_output = float_input
+
+    return float_output
