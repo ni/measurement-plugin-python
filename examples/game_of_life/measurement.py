d38b13763a06501e3672dd2db3f682689e4ea8d0
Joel Dixon <joel.dixon@ni.com>
2023-09-19 20:16:18 -0500

    Add max_generations so the game of life will stop and is more useful in TestStand

--- examples/game_of_life/measurement.py
+++ examples/game_of_life/measurement.py
@@ -24,15 +24,17 @@ Grid = List[List[bool]]
 @measurement_service.configuration("width", nims.DataType.UInt32, 100)
 @measurement_service.configuration("height", nims.DataType.UInt32, 100)
 @measurement_service.configuration("update_interval", nims.DataType.UInt32, 100)
+@measurement_service.configuration("max_generations", nims.DataType.Int32, -1)
 @measurement_service.output("game_of_life", nims.DataType.DoubleXYData)
 @measurement_service.output("generation", nims.DataType.UInt32)
 def measure(
-    width: int, height: int, update_interval: int
+    width: int, height: int, update_interval: int, max_generations: int
 ) -> Generator[Tuple[xydata_pb2.DoubleXYData, int], None, None]:
     """Streaming measurement that returns Conway's Game of Life grid as DoubleXYData."""
     grid = _initialize_grid_with_seeded_data(width, height)
     generation = 0
-    while True:
+    # either go forever if max_generations < 0 or until max_generations is reached
+    while max_generations < 0 or generation < max_generations:
         generation += 1
 
         xydata = _initialize_xydata_and_frame(width, height)
