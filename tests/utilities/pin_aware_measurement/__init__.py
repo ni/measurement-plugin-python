13232d5d0ae3b47e804894e935b73c03699057c0
Brad Keryan <brad.keryan@ni.com>
2023-09-22 14:40:18 -0500

    tests: Add pin_aware_measurement

---
+++ tests/utilities/pin_aware_measurement/__init__.py
@@ -0,0 +1,48 @@
+"""Pin-aware MeasurementLink test service."""
+import pathlib
+from typing import Iterable, Tuple
+
+import ni_measurementlink_service as nims
+
+service_directory = pathlib.Path(__file__).resolve().parent
+measurement_service = nims.MeasurementService(
+    service_config_path=service_directory / "PinAwareMeasurement.serviceconfig",
+    version="0.1.0.0",
+    ui_file_paths=[
+        service_directory,
+    ],
+)
+
+
+@measurement_service.register_measurement
+@measurement_service.configuration("pin_names", nims.DataType.PinArray1D, ["Pin1"])
+@measurement_service.configuration("multi_session", nims.DataType.Boolean, False)
+@measurement_service.output("pin_map_id", nims.DataType.String)
+@measurement_service.output("sites", nims.DataType.Int32Array1D)
+@measurement_service.output("session_names", nims.DataType.StringArray1D)
+@measurement_service.output("resource_names", nims.DataType.StringArray1D)
+@measurement_service.output("channel_lists", nims.DataType.StringArray1D)
+def measure(
+    pin_names: Iterable[str],
+    multi_session: bool,
+) -> Tuple[str, Iterable[int], Iterable[str], Iterable[str], Iterable[str]]:
+    """Pin-aware MeasurementLink test service."""
+    pin_map_context = measurement_service.context.pin_map_context
+    if multi_session:
+        with measurement_service.context.reserve_sessions(pin_names, timeout=5.0) as reservation:
+            return (
+                pin_map_context.pin_map_id,
+                pin_map_context.sites,
+                [s.session_name for s in reservation.session_info],
+                [s.resource_name for s in reservation.session_info],
+                [s.channel_list for s in reservation.session_info],
+            )
+    else:
+        with measurement_service.context.reserve_session(pin_names, timeout=5.0) as reservation:
+            return (
+                pin_map_context.pin_map_id,
+                pin_map_context.sites,
+                [reservation.session_info.resource_name],
+                [reservation.session_info.resource_name],
+                [reservation.session_info.channel_list],
+            )
