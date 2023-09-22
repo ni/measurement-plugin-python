ab2529934cc658903dc6d737afa6aba79505c746
Sam Chrisvin <104556074+samchris007@users.noreply.github.com>
2023-09-22 18:26:24 +0530

    Rename test methods

--- tests/unit/test_discovery_client.py
+++ tests/unit/test_discovery_client.py
@@ -228,7 +228,7 @@ def test___get_discovery_service_address___start_service_jit___returns_expected_
     assert _TEST_SERVICE_PORT in discovery_service_address
 
 
-def test___open_key_file_that_throws_ioerror___get_discovery_service_address___throws_timeouterror(
+def test___get_discovery_service_address___open_key_file_throws_ioerror___throws_timeouterror(
     mocker: MockerFixture,
     temp_discovery_key_file_path: pathlib.Path,
     temp_registration_json_file_path: pathlib.Path,
@@ -254,7 +254,7 @@ def test___open_key_file_that_throws_ioerror___get_discovery_service_address___t
     assert exc_info.type is TimeoutError
 
 
-def test___open_key_file_that_throws_windowserror___get_discovery_service_address___throws_timeouterror(
+def test___get_discovery_service_address___open_key_file_throws_windowserror___throws_timeouterror(
     mocker: MockerFixture,
     temp_discovery_key_file_path: pathlib.Path,
     temp_registration_json_file_path: pathlib.Path,
