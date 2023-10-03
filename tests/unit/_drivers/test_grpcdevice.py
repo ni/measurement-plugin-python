5af920fc79bead7d7f6820d5455136f966544401
Brad Keryan <brad.keryan@ni.com>
2023-10-03 12:37:30 -0500

    tests: Update grpcdevice tests

--- tests/unit/_drivers/test_grpcdevice.py
+++ tests/unit/_drivers/test_grpcdevice.py
@@ -1,6 +1,6 @@
 from unittest.mock import Mock
 
-import pytest
+from pytest_mock import MockerFixture
 
 from ni_measurementlink_service._drivers._grpcdevice import (
     SERVICE_CLASS,
@@ -10,7 +10,7 @@ from ni_measurementlink_service._internal.discovery_client import ServiceLocatio
 from tests.utilities import fake_driver
 
 
-def test___valid_driver_module___get_insecure_grpc_device_channel____returns_channel(
+def test___default_configuration___get_insecure_grpc_device_channel___service_discovered_and_channel_returned(
     discovery_client: Mock,
     grpc_channel: Mock,
     grpc_channel_pool: Mock,
@@ -30,11 +30,35 @@ def test___valid_driver_module___get_insecure_grpc_device_channel____returns_cha
     assert returned_channel is grpc_channel
 
 
-def test___invalid_driver_module___get_insecure_grpc_device_channel____raises_type_error(
+def test___use_grpc_device_server_false___get_insecure_grpc_device_channel___none_returned(
     discovery_client: Mock,
     grpc_channel_pool: Mock,
+    mocker: MockerFixture,
 ) -> None:
-    discovery_client.resolve_service.return_value = ServiceLocation("localhost", "1234", "")
+    mocker.patch("ni_measurementlink_service._drivers._grpcdevice.USE_GRPC_DEVICE_SERVER", False)
 
-    with pytest.raises(TypeError):
-        _ = get_insecure_grpc_device_channel(discovery_client, grpc_channel_pool, pytest)  # type: ignore[arg-type]
+    returned_channel = get_insecure_grpc_device_channel(
+        discovery_client, grpc_channel_pool, fake_driver.GRPC_SERVICE_INTERFACE_NAME
+    )
+
+    assert returned_channel is None
+
+
+def test___grpc_device_address_set___get_insecure_grpc_device_channel___address_used_and_channel_returned(
+    discovery_client: Mock,
+    grpc_channel: Mock,
+    grpc_channel_pool: Mock,
+    mocker: MockerFixture,
+) -> None:
+    mocker.patch(
+        "ni_measurementlink_service._drivers._grpcdevice.GRPC_DEVICE_ADDRESS", "[::1]:31763"
+    )
+    grpc_channel_pool.get_channel.return_value = grpc_channel
+
+    returned_channel = get_insecure_grpc_device_channel(
+        discovery_client, grpc_channel_pool, fake_driver.GRPC_SERVICE_INTERFACE_NAME
+    )
+
+    discovery_client.resolve_service.assert_not_called()
+    grpc_channel_pool.get_channel.assert_called_with("[::1]:31763")
+    assert returned_channel is grpc_channel
