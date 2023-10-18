0e3668d2e34d12819bac18539c9a0328b8184738
Sam Chrisvin <104556074+samchris007@users.noreply.github.com>
2023-10-18 14:30:18 +0530

    Split the teststand code modules for niswitch and nivisadmm examples

--- examples/niswitch_control_relays/teststand_niswitch.py
+++ examples/niswitch_control_relays/teststand_niswitch.py
@@ -3,30 +3,8 @@ from typing import Any
 
 import ni_measurementlink_service as nims
 import niswitch
-from _helpers import GrpcChannelPoolHelper, PinMapClient, TestStandSupport
+from _helpers import GrpcChannelPoolHelper, TestStandSupport
 from _niswitch_helpers import create_session
-
-
-def update_pin_map(pin_map_path: str, sequence_context: Any) -> None:
-    """Update registered pin map contents.
-
-    Create and register a pin map if a pin map resource for the specified pin map id is not found.
-
-    Args:
-        pin_map_path:
-            An absolute or relative path to the pin map file.
-        sequence_context:
-            The SequenceContext COM object from the TestStand sequence execution.
-            (Dynamically typed.)
-    """
-    teststand_support = TestStandSupport(sequence_context)
-    pin_map_abs_path = teststand_support.resolve_file_path(pin_map_path)
-
-    with GrpcChannelPoolHelper() as grpc_channel_pool:
-        pin_map_client = PinMapClient(grpc_channel=grpc_channel_pool.pin_map_channel)
-        pin_map_id = pin_map_client.update_pin_map(pin_map_abs_path)
-
-    teststand_support.set_active_pin_map_id(pin_map_id)
 
 
 def create_niswitch_sessions(sequence_context: Any) -> None:
