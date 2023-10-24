c3395f1e2963722c685744da972322c33912fff4
Jayaseelan James <jayaseelan.james@ni.com>
2023-10-24 17:14:11 +0530

    tests: add unit tests for closing_session api

--- tests/utilities/fake_driver.py
+++ tests/utilities/fake_driver.py
@@ -2,13 +2,13 @@
 from __future__ import annotations
 
 import sys
-from enum import Enum, IntEnum
+from enum import Enum
 from types import TracebackType
 from typing import TYPE_CHECKING, Any, ContextManager, Dict, Optional, Type
 
-if TYPE_CHECKING:
-    import grpc
+from ni_measurementlink_service.session_management._types import SessionInitializationBehavior
 
+if TYPE_CHECKING:
     if sys.version_info >= (3, 11):
         from typing import Self
     else:
@@ -17,34 +17,11 @@ if TYPE_CHECKING:
 
 GRPC_SERVICE_INTERFACE_NAME = "nifake_grpc.NiFake"
 
-# The GrpcSessionOptions classes in nimi-python and nidaqmx-python have an api_key field.
-_API_KEY = "00000000-0000-0000-0000-000000000000"
-
-
-class SessionInitializationBehavior(IntEnum):
-    """Specifies whether to initialize a new session or attach to an existing session."""
-
-    AUTO = 0
-    INITIALIZE_SERVER_SESSION = 1
-    ATTACH_TO_SERVER_SESSION = 2
-
-
-class GrpcSessionOptions:
-    """gRPC session options."""
-
-    def __init__(
-        self,
-        grpc_channel: grpc.Channel,
-        session_name: str,
-        *,
-        api_key: str = _API_KEY,
-        initialization_behavior: SessionInitializationBehavior = SessionInitializationBehavior.AUTO,
-    ) -> None:
-        """Initialize the gRPC session options."""
-        self.grpc_channel = grpc_channel
-        self.session_name = session_name
-        self.api_key = api_key
-        self.initialization_behavior = initialization_behavior
+_CLOSE_BEHAVIORS = [
+    SessionInitializationBehavior.AUTO,
+    SessionInitializationBehavior.INITIALIZE_SERVER_SESSION,
+    SessionInitializationBehavior.ATTACH_TO_SESSION_THEN_CLOSE,
+]
 
 
 class MeasurementType(Enum):
@@ -96,6 +73,14 @@ class _SessionBase:
         return 0.0
 
 
+class ClosableSession(_SessionBase):
+    """A driver session that supports close()."""
+
+    def close(self) -> None:
+        """Close the session."""
+        self.is_closed = True
+
+
 class ContextManagerSession(_SessionBase):
     """A driver session that supports the context manager protocol."""
 
@@ -110,10 +95,6 @@ class ContextManagerSession(_SessionBase):
         traceback: Optional[TracebackType],
     ) -> None:
         """Exit the session's runtime context."""
-        self.close()
-
-    def close(self) -> None:
-        """Close the session."""
         self.is_closed = True
 
 
@@ -141,4 +122,8 @@ class Session(_SessionBase):
         traceback: Optional[TracebackType],
     ) -> None:
         """Exit the session's runtime context."""
-        self.close()
+        if (
+            "initialization_behavior" not in self.options
+            or self.options["initialization_behavior"] in _CLOSE_BEHAVIORS
+        ):
+            self.close()
