36203fb8329b8b3cf9ae0068fd427510310a9aec
Jayaseelan James <jayaseelan.james@ni.com>
2023-10-26 21:30:02 +0530

    update: nidigital teststand code module

--- examples/nidigital_spi/teststand_nidigital.py
+++ examples/nidigital_spi/teststand_nidigital.py
@@ -1,13 +1,16 @@
 """Functions to set up and tear down sessions of NI Digital Pattern instruments in NI TestStand."""
 from typing import Any, Iterable
 
-import ni_measurementlink_service as nims
-import nidigital
 from _helpers import GrpcChannelPoolHelper, TestStandSupport
-from _nidigital_helpers import create_session
+
+from ni_measurementlink_service.discovery import DiscoveryClient
 from ni_measurementlink_service.session_management import (
     INSTRUMENT_TYPE_NI_DIGITAL_PATTERN,
+    PinMapContext,
+    SessionInitializationBehavior,
+    SessionManagementClient,
 )
+from ni_measurementlink_service.session_management._reservation import MultiSessionReservation
 
 
 def create_nidigital_sessions(sequence_context: Any) -> None:
@@ -21,15 +24,17 @@ def create_nidigital_sessions(sequence_context: Any) -> None:
     pin_map_id = teststand_support.get_active_pin_map_id()
 
     with GrpcChannelPoolHelper() as grpc_channel_pool:
-        session_management_client = nims.session_management.Client(
-            grpc_channel=grpc_channel_pool.session_management_channel
+        discovery_client = DiscoveryClient(grpc_channel_pool=grpc_channel_pool)
+        session_management_client = SessionManagementClient(
+            discovery_client=discovery_client, grpc_channel_pool=grpc_channel_pool
         )
         with _reserve_sessions(
             session_management_client, pin_map_id, INSTRUMENT_TYPE_NI_DIGITAL_PATTERN
         ) as reservation:
-            for session_info in reservation.session_info:
-                # Leave session open.
-                _ = _create_new_nidigital_session(grpc_channel_pool, session_info)
+            with reservation.initialize_nidigital_sessions(
+                initialization_behavior=SessionInitializationBehavior.INITIALIZE_SESSION_THEN_DETACH
+            ):
+                pass
 
             session_management_client.register_sessions(reservation.session_info)
 
@@ -47,15 +52,18 @@ def load_nidigital_pin_map(pin_map_path: str, sequence_context: Any) -> None:
     pin_map_abs_path = teststand_support.resolve_file_path(pin_map_path)
 
     with GrpcChannelPoolHelper() as grpc_channel_pool:
-        session_management_client = nims.session_management.Client(
-            grpc_channel=grpc_channel_pool.session_management_channel
+        discovery_client = DiscoveryClient(grpc_channel_pool=grpc_channel_pool)
+        session_management_client = SessionManagementClient(
+            discovery_client=discovery_client, grpc_channel_pool=grpc_channel_pool
         )
         with _reserve_sessions(
             session_management_client, pin_map_id, INSTRUMENT_TYPE_NI_DIGITAL_PATTERN
         ) as reservation:
-            for session_info in reservation.session_info:
-                with _attach_nidigital_session(grpc_channel_pool, session_info) as session:
-                    session.load_pin_map(pin_map_abs_path)
+            with reservation.initialize_nidigital_sessions(
+                initialization_behavior=SessionInitializationBehavior.ATTACH_TO_SERVER_SESSION
+            ) as session_infos:
+                for session_info in session_infos:
+                    session_info.session.load_pin_map(pin_map_abs_path)
 
 
 def load_nidigital_specifications_levels_and_timing(
@@ -82,15 +90,18 @@ def load_nidigital_specifications_levels_and_timing(
     timing_file_abs_paths = [teststand_support.resolve_file_path(p) for p in timing_file_paths]
 
     with GrpcChannelPoolHelper() as grpc_channel_pool:
-        session_management_client = nims.session_management.Client(
-            grpc_channel=grpc_channel_pool.session_management_channel
+        discovery_client = DiscoveryClient(grpc_channel_pool=grpc_channel_pool)
+        session_management_client = SessionManagementClient(
+            discovery_client=discovery_client, grpc_channel_pool=grpc_channel_pool
         )
         with _reserve_sessions(
             session_management_client, pin_map_id, INSTRUMENT_TYPE_NI_DIGITAL_PATTERN
         ) as reservation:
-            for session_info in reservation.session_info:
-                with _attach_nidigital_session(grpc_channel_pool, session_info) as session:
-                    session.load_specifications_levels_and_timing(
+            with reservation.initialize_nidigital_sessions(
+                initialization_behavior=SessionInitializationBehavior.ATTACH_TO_SERVER_SESSION
+            ) as session_infos:
+                for session_info in session_infos:
+                    session_info.session.load_specifications_levels_and_timing(
                         specifications_file_abs_paths, levels_file_abs_paths, timing_file_abs_paths
                     )
 
@@ -111,69 +122,48 @@ def load_nidigital_patterns(
     pattern_file_abs_paths = [teststand_support.resolve_file_path(p) for p in pattern_file_paths]
 
     with GrpcChannelPoolHelper() as grpc_channel_pool:
-        session_management_client = nims.session_management.Client(
-            grpc_channel=grpc_channel_pool.session_management_channel
+        discovery_client = DiscoveryClient(grpc_channel_pool=grpc_channel_pool)
+        session_management_client = SessionManagementClient(
+            discovery_client=discovery_client, grpc_channel_pool=grpc_channel_pool
         )
         with _reserve_sessions(
             session_management_client, pin_map_id, INSTRUMENT_TYPE_NI_DIGITAL_PATTERN
         ) as reservation:
-            for session_info in reservation.session_info:
-                with _attach_nidigital_session(grpc_channel_pool, session_info) as session:
+            with reservation.initialize_nidigital_sessions(
+                initialization_behavior=SessionInitializationBehavior.ATTACH_TO_SERVER_SESSION
+            ) as session_infos:
+                for session_info in session_infos:
                     for pattern_file_abs_path in pattern_file_abs_paths:
-                        session.load_pattern(pattern_file_abs_path)
+                        session_info.session.load_pattern(pattern_file_abs_path)
 
 
 def destroy_nidigital_sessions() -> None:
     """Destroy and unregister all NI-Digital sessions."""
     with GrpcChannelPoolHelper() as grpc_channel_pool:
-        session_management_client = nims.session_management.Client(
-            grpc_channel=grpc_channel_pool.session_management_channel
+        discovery_client = DiscoveryClient(grpc_channel_pool=grpc_channel_pool)
+        session_management_client = SessionManagementClient(
+            discovery_client=discovery_client, grpc_channel_pool=grpc_channel_pool
         )
         with session_management_client.reserve_all_registered_sessions(
-            instrument_type_id=nims.session_management.INSTRUMENT_TYPE_NI_DIGITAL_PATTERN,
+            instrument_type_id=INSTRUMENT_TYPE_NI_DIGITAL_PATTERN,
         ) as reservation:
-            session_management_client.unregister_sessions(reservation.session_info)
+            if not reservation.session_info:
+                return
 
-            for session_info in reservation.session_info:
-                session = _attach_nidigital_session(grpc_channel_pool, session_info)
-                session.close()
+            session_management_client.unregister_sessions(reservation.session_info)
+            with reservation.initialize_nidigital_sessions(
+                initialization_behavior=SessionInitializationBehavior.ATTACH_TO_SESSION_THEN_CLOSE
+            ):
+                pass
 
 
 def _reserve_sessions(
-    session_management_client: nims.session_management.Client,
+    session_management_client: SessionManagementClient,
     pin_map_id: str,
     instrument_type_id: str,
-) -> nims.session_management.MultiSessionReservation:
-    pin_map_context = nims.session_management.PinMapContext(pin_map_id=pin_map_id, sites=None)
+) -> MultiSessionReservation:
+    pin_map_context = PinMapContext(pin_map_id=pin_map_id, sites=None)
 
     return session_management_client.reserve_sessions(
-        context=pin_map_context, instrument_type_id=instrument_type_id
-    )
-
-
-def _create_new_nidigital_session(
-    grpc_channel_pool: GrpcChannelPoolHelper,
-    session_info: nims.session_management.SessionInformation,
-) -> nidigital.Session:
-    grpc_device_channel = grpc_channel_pool.get_grpc_device_channel(
-        nidigital.GRPC_SERVICE_INTERFACE_NAME
-    )
-    return create_session(
-        session_info,
-        grpc_device_channel,
-        initialization_behavior=nidigital.SessionInitializationBehavior.INITIALIZE_SERVER_SESSION,
-    )
-
-
-def _attach_nidigital_session(
-    grpc_channel_pool: GrpcChannelPoolHelper,
-    session_info: nims.session_management.SessionInformation,
-) -> nidigital.Session:
-    grpc_device_channel = grpc_channel_pool.get_grpc_device_channel(
-        nidigital.GRPC_SERVICE_INTERFACE_NAME
-    )
-    return create_session(
-        session_info,
-        grpc_device_channel,
-        initialization_behavior=nidigital.SessionInitializationBehavior.ATTACH_TO_SERVER_SESSION,
+        pin_map_context, instrument_type_id=instrument_type_id
     )
