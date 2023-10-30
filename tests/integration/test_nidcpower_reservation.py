ab94562459bc4ac9f7c6fc2625a79f4005947bfa
Jayaseelan James <jayaseelan.james@ni.com>
2023-10-30 19:32:16 +0530

    tests: simplify nidcpower integration test

--- tests/integration/test_nidcpower_reservation.py
+++ tests/integration/test_nidcpower_reservation.py
@@ -8,27 +8,23 @@ from ni_measurementlink_service.session_management import (
 )
 from tests.utilities.pin_map_client import PinMapClient
 
-_PIN_MAP_A = "PinMapA_3Instruments_3DutPins_2SystemPins_2Sites.pinmap"
-_PIN_MAP_A_NIDCPOWER_PIN_NAMES = ["A", "B", "C"]
-
 
 def test___single_session_reserved___initialize_nidcpower_session___single_session_created(
     pin_map_client: PinMapClient,
     pin_map_directory: pathlib.Path,
     session_management_client: SessionManagementClient,
 ) -> None:
-    pin_map_id = pin_map_client.update_pin_map(pin_map_directory / _PIN_MAP_A)
+    pin_map_name = "1Smu1ChannelGroup1Pin1Site.pinmap"
+    pin_names = ["Pin1"]
+    pin_map_id = pin_map_client.update_pin_map(pin_map_directory / pin_map_name)
     pin_map_context = PinMapContext(pin_map_id=pin_map_id, sites=[0])
     with session_management_client.reserve_session(
-        pin_map_context, _PIN_MAP_A_NIDCPOWER_PIN_NAMES[0], timeout=0
+        pin_map_context, pin_names, timeout=0
     ) as reservation:
         with reservation.initialize_nidcpower_session() as session_info:
             assert session_info.session is not None
 
-        assert (
-            session_info.session_name
-            == "DCPower1/0, DCPower1/1, DCPower1/2, DCPower1/3, DCPower2/1"
-        )
+        assert session_info.session_name == "DCPower1/0"
 
 
 @pytest.fixture
