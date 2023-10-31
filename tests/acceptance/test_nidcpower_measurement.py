da45dafe217bc473eb340792c72e3c50748cd1c9
Jayaseelan James <jayaseelan.james@ni.com>
2023-10-31 13:58:05 +0530

    tests: add more acceptance tests

--- tests/acceptance/test_nidcpower_measurement.py
+++ tests/acceptance/test_nidcpower_measurement.py
@@ -1,5 +1,5 @@
 import pathlib
-from typing import Generator
+from typing import Generator, Iterable, NamedTuple
 
 import pytest
 
@@ -13,7 +13,7 @@ from ni_measurementlink_service._internal.stubs.ni.measurementlink.pin_map_conte
     PinMapContext,
 )
 from ni_measurementlink_service.measurement.service import MeasurementService
-from tests.assets import nidcpower_measurement_pb2
+from tests.assets.nidcpower_measurement_pb2 import NIDCPowerConfigurations, NIDCPowerOutputs
 from tests.utilities import nidcpower_measurement
 from tests.utilities.pin_map_client import PinMapClient
 
@@ -27,28 +27,43 @@ def test___single_session___measure___single_session_created(
     pin_names = ["Pin1"]
     pin_map_id = pin_map_client.update_pin_map(pin_map_directory / pin_map_name)
     pin_map_context = PinMapContext(pin_map_id=pin_map_id, sites=[0])
-    configurations = nidcpower_measurement_pb2.NIDCPowerConfigurations(
-        pin_names=pin_names, multi_session=False
-    )
+    configurations = NIDCPowerConfigurations(pin_names=pin_names, multi_session=False)
 
     outputs = _measure(stub_v2, pin_map_context, configurations)
 
-    assert outputs.session_names == ["DCPower1/0"]
-    assert outputs.resource_names == ["DCPower1/0"]
-    assert outputs.channel_lists == ["DCPower1/0"]
+    assert _get_output(outputs) == [_MeasurementOutput("DCPower1/0", "DCPower1/0", "DCPower1/0")]
+
+
+def test___multiple_sessions___measure___multiple_sessions_created(
+    pin_map_client: PinMapClient,
+    pin_map_directory: pathlib.Path,
+    stub_v2: MeasurementServiceStub,
+) -> None:
+    pin_map_name = "1Smu2ChannelGroup2Pin1Site.pinmap"
+    pin_names = ["Pin1", "Pin2"]
+    pin_map_id = pin_map_client.update_pin_map(pin_map_directory / pin_map_name)
+    pin_map_context = PinMapContext(pin_map_id=pin_map_id, sites=[0])
+    configurations = NIDCPowerConfigurations(pin_names=pin_names, multi_session=True)
+
+    outputs = _measure(stub_v2, pin_map_context, configurations)
+
+    assert _get_output(outputs) == [
+        _MeasurementOutput("DCPower1/0", "DCPower1/0", "DCPower1/0"),
+        _MeasurementOutput("DCPower1/2", "DCPower1/2", "DCPower1/2"),
+    ]
 
 
 def _measure(
     stub_v2: MeasurementServiceStub,
     pin_map_context: PinMapContext,
-    configurations: nidcpower_measurement_pb2.NIDCPowerConfigurations,
-) -> nidcpower_measurement_pb2.NIDCPowerOutputs:
+    configurations: NIDCPowerConfigurations,
+) -> NIDCPowerOutputs:
     request = MeasureRequest(pin_map_context=pin_map_context)
     request.configuration_parameters.Pack(configurations)
     response_iterator = stub_v2.Measure(request)
     responses = list(response_iterator)
     assert len(responses) == 1
-    outputs = nidcpower_measurement_pb2.NIDCPowerOutputs.FromString(responses[0].outputs.value)
+    outputs = NIDCPowerOutputs.FromString(responses[0].outputs.value)
     return outputs
 
 
@@ -63,3 +78,21 @@ def measurement_service() -> Generator[MeasurementService, None, None]:
 def pin_map_directory(test_assets_directory: pathlib.Path) -> pathlib.Path:
     """Test fixture that returns the pin map directory."""
     return test_assets_directory / "acceptance" / "session_management"
+
+
+class _MeasurementOutput(NamedTuple):
+    session_name: str
+    resource_name: str
+    channel_list: str
+
+
+def _get_output(
+    outputs: NIDCPowerOutputs,
+) -> Iterable[_MeasurementOutput]:
+    measurement_output = []
+    for session_name, resource_name, channel_list in zip(
+        outputs.session_names, outputs.resource_names, outputs.channel_lists
+    ):
+        measurement_output.append(_MeasurementOutput(session_name, resource_name, channel_list))
+
+    return measurement_output
