9847cd2b5b07849e1e8b11f525d964bdae967d33
Jayaseelan James <jayaseelan.james@ni.com>
2023-11-07 12:23:45 +0530

    tests: add wait until logic for non-simulation

--- tests/utilities/nifgen_measurement/__init__.py
+++ tests/utilities/nifgen_measurement/__init__.py
@@ -4,9 +4,11 @@ import time
 from contextlib import ExitStack
 from typing import Iterable, Sequence, Tuple
 
+import hightime
 import nifgen
 
 import ni_measurementlink_service as nims
+from ni_measurementlink_service.session_management import TypedSessionInformation
 
 service_directory = pathlib.Path(__file__).resolve().parent
 measurement_service = nims.MeasurementService(
@@ -29,13 +31,12 @@ def measure(
     pin_names: Iterable[str], multi_session: bool
 ) -> Tuple[Iterable[str], Iterable[str], Iterable[str], Iterable[str]]:
     """NI-Fgen MeasurementLink test service."""
-    nifgen_waveform = nifgen.Waveform.SINE
     if multi_session:
         with measurement_service.context.reserve_sessions(pin_names) as reservation:
             with reservation.initialize_nifgen_sessions() as session_infos:
                 assert all([session_info.session is not None for session_info in session_infos])
                 connections = reservation.get_nifgen_connections(pin_names)
-                _generate_standard_waveform(session_infos, nifgen_waveform)
+                _generate_standard_waveform(session_infos)
 
                 return (
                     [session_info.session_name for session_info in session_infos],
@@ -48,7 +49,7 @@ def measure(
             with reservation.initialize_nifgen_session() as session_info:
                 assert session_info.session is not None
                 connection = reservation.get_nifgen_connection(list(pin_names)[0])
-                _generate_standard_waveform([session_info], nifgen_waveform)
+                _generate_standard_waveform([session_info])
 
                 return (
                     [session_info.resource_name],
@@ -59,16 +60,30 @@ def measure(
 
 
 def _generate_standard_waveform(
-    session_infos: Sequence[nims.session_management.TypedSessionInformation[nifgen.Session]],
-    nifgen_waveform: nifgen.Waveform,
+    session_infos: Sequence[TypedSessionInformation[nifgen.Session]],
 ) -> None:
     for session_info in session_infos:
         session_info.session.output_mode = nifgen.OutputMode.FUNC
         channels = session_info.session.channels[session_info.channel_list]
-        channels.configure_standard_waveform(nifgen_waveform, 2.0, 1.0e6)
+        channels.configure_standard_waveform(nifgen.Waveform.SINE, 2.0, 1.0e6)
 
     with ExitStack() as stack:
         for session_info in session_infos:
             stack.enter_context(session_info.session.initiate())
 
+    sessions = [session_info.session for session_info in session_infos]
+    is_simulated = any(session.simulate for session in sessions)
+
+    if is_simulated:
         time.sleep(100e-3)
+    else:
+        try:
+            stop_time = time.time() + 10
+            for session in sessions:
+                remaining_time = max(stop_time - time.time(), 0.0)
+                sleep_time = min(remaining_time, 100e-3)
+                session.wait_until_done(hightime.timedelta(seconds=sleep_time))
+        except nifgen.errors.DriverError as e:
+            if e.code in [-1074098044, -1074118637]:
+                pass
+            raise
