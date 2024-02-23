00dbb266af232fd79bbb5afe283925843ee3e701
Jayaseelan James <jayaseelan.james@ni.com>
2024-02-23 15:24:32 +0530

    tests: add acceptance test for niswitch multiplexer

---
+++ tests/acceptance/test_niswitch_multiplexer_measurement.py
@@ -0,0 +1,95 @@
+import pathlib
+from typing import Generator, Iterable, NamedTuple
+
+import pytest
+
+from ni_measurementlink_service._internal.stubs.ni.measurementlink.measurement.v2.measurement_service_pb2 import (
+    MeasureRequest,
+)
+from ni_measurementlink_service._internal.stubs.ni.measurementlink.measurement.v2.measurement_service_pb2_grpc import (
+    MeasurementServiceStub,
+)
+from ni_measurementlink_service._internal.stubs.ni.measurementlink.pin_map_context_pb2 import (
+    PinMapContext,
+)
+from ni_measurementlink_service.measurement.service import MeasurementService
+from tests.utilities.measurements import niswitch_multiplexer_measurement
+from tests.utilities.pin_map_client import PinMapClient
+from tests.utilities.stubs.niswitchmultiplexer.types_pb2 import Configurations, Outputs
+
+_SITE = 0
+
+
+def test___single_session___measure___creates_single_session(
+    pin_map_context: PinMapContext,
+    stub_v2: MeasurementServiceStub,
+) -> None:
+    configurations = Configurations(pin_names=["A"], multi_session=False)
+
+    outputs = _measure(stub_v2, pin_map_context, configurations)
+
+    assert _get_output(outputs) == [
+        _MeasurementOutput("Multiplexer1", "Multiplexer1", "b0c1->b0r0", "DCPower1/0")
+    ]
+
+
+def test___multiple_sessions___measure___creates_multiple_sessions(
+    pin_map_context: PinMapContext,
+    stub_v2: MeasurementServiceStub,
+) -> None:
+    configurations = Configurations(pin_names=["A", "B"], multi_session=True)
+
+    outputs = _measure(stub_v2, pin_map_context, configurations)
+
+    assert _get_output(outputs) == [
+        _MeasurementOutput("Multiplexer1", "Multiplexer1", "b0c1->b0r0", "DCPower1/0"),
+        _MeasurementOutput("Multiplexer2", "Multiplexer2", "b0c3->b0r0", "DCPower1/2"),
+    ]
+
+
+def _measure(
+    stub_v2: MeasurementServiceStub,
+    pin_map_context: PinMapContext,
+    configurations: Configurations,
+) -> Outputs:
+    request = MeasureRequest(pin_map_context=pin_map_context)
+    request.configuration_parameters.Pack(configurations)
+    response_iterator = stub_v2.Measure(request)
+    responses = list(response_iterator)
+    assert len(responses) == 1
+    outputs = Outputs.FromString(responses[0].outputs.value)
+    return outputs
+
+
+@pytest.fixture(scope="module")
+def measurement_service() -> Generator[MeasurementService, None, None]:
+    """Test fixture that creates and hosts a measurement service."""
+    with niswitch_multiplexer_measurement.measurement_service.host_service() as service:
+        yield service
+
+
+@pytest.fixture
+def pin_map_context(pin_map_client: PinMapClient, pin_map_directory: pathlib.Path) -> PinMapContext:
+    pin_map_name = "2SwitchMultiplexer1Smu2Pin1Site.pinmap"
+    pin_map_id = pin_map_client.update_pin_map(pin_map_directory / pin_map_name)
+
+    return PinMapContext(pin_map_id=pin_map_id, sites=[_SITE])
+
+
+class _MeasurementOutput(NamedTuple):
+    multiplexer_session_name: str
+    multiplexer_resource_name: str
+    multiplexer_route: str
+    connected_channel: str
+
+
+def _get_output(outputs: Outputs) -> Iterable[_MeasurementOutput]:
+    return [
+        _MeasurementOutput(session_name, resource_name, multiplexer_route, connected_channel)
+        for session_name, resource_name, multiplexer_route, connected_channel in zip(
+            outputs.multiplexer_session_names,
+            outputs.multiplexer_resource_names,
+            outputs.multiplexer_routes,
+            outputs.connected_channels,
+        )
+    ]
