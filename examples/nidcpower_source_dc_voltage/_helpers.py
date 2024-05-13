346ac3e88c686e2fc6c8ce22be7ce660ba52f71e
Prathana.Mahendran <prathana.mahendran@ni.com>
2024-05-13 09:23:18 +0530

    CHange the variable name

--- examples/nidcpower_source_dc_voltage/_helpers.py
+++ examples/nidcpower_source_dc_voltage/_helpers.py
@@ -10,6 +10,8 @@ import click
 class TestStandSupport(object):
     """Class that communicates with TestStand."""
 
+    PIN_MAP_ID_VAR = "NI.MeasurementLink.PinMapId"
+
     def __init__(self, sequence_context: Any) -> None:
         """Initialize the TestStandSupport object.
 
@@ -26,14 +28,13 @@ class TestStandSupport(object):
         Returns:
             The resource id of the pin map that is registered to the pin map service.
         """
-        pin_map_id = "NI.MeasurementLink.PinMapId"
         run_time_variables = self._sequence_context.Execution.RunTimeVariables
-        if not run_time_variables.Exists(pin_map_id, 0x0):
+        if not run_time_variables.Exists(self.PIN_MAP_ID_VAR, 0x0):
             raise RuntimeError(
                 "Failed to retrieve the registered pin map ID. Possible reason: The sequence might not include an 'Update Pin Map' step."
             )
-        return run_time_variables.GetValString(pin_map_id, 0x0)
-    
+        return run_time_variables.GetValString(self.PIN_MAP_ID_VAR, 0x0)
+
     def resolve_file_path(self, file_path: str) -> str:
         """Resolve the absolute path to a file using the TestStand search directories.
 
