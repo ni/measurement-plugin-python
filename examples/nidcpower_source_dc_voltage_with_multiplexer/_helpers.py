437d4a54170ae7b8df531167069b802cd8e4383b
prat-mahendran <137057078+prat-mahendran@users.noreply.github.com>
2024-05-14 09:54:43 +0530

    Tech Debt : Display proper error message when update pin map step is… (#719)
    * Tech Debt : Display proper error message when update pin map step is missing
    
    * Rectify pipeline errors
    
    * Fix :Update generator file
    
    * Add : Efficient check
    
    * Modify generator files
    
    * Remove unwanted XML files
    
    * Redo the sequence file
    
    * Fix ;PR Comments
    
    * Remove XML
    
    * Fix:PR Isssues
    
    * Add constants
    
    * Change the cases
    
    * Add example DAQmx
    
    * CHange the variable name
    
    * Rename python local variable

--- examples/nidcpower_source_dc_voltage_with_multiplexer/_helpers.py
+++ examples/nidcpower_source_dc_voltage_with_multiplexer/_helpers.py
@@ -10,6 +10,8 @@ import click
 class TestStandSupport(object):
     """Class that communicates with TestStand."""
 
+    _PIN_MAP_ID_VAR = "NI.MeasurementLink.PinMapId"
+
     def __init__(self, sequence_context: Any) -> None:
         """Initialize the TestStandSupport object.
 
@@ -26,9 +28,12 @@ class TestStandSupport(object):
         Returns:
             The resource id of the pin map that is registered to the pin map service.
         """
-        return self._sequence_context.Execution.RunTimeVariables.GetValString(
-            "NI.MeasurementLink.PinMapId", 0x0
-        )
+        run_time_variables = self._sequence_context.Execution.RunTimeVariables
+        if not run_time_variables.Exists(self._PIN_MAP_ID_VAR, 0x0):
+            raise RuntimeError(
+                "Failed to retrieve the registered pin map ID. Possible reason: The sequence might not include an 'Update Pin Map' step."
+            )
+        return run_time_variables.GetValString(self._PIN_MAP_ID_VAR, 0x0)
 
     def resolve_file_path(self, file_path: str) -> str:
         """Resolve the absolute path to a file using the TestStand search directories.
