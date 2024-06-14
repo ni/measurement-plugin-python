0faf27d8e88c5bcd083fe442a143069d0750f37c
Luke Schroeder <luke.schroeder@ni.com>
2024-06-14 13:47:56 -0500

    Fixup NISHAREDDIR -> NISHAREDDIR64
    Signed-off-by: Luke Schroeder <luke.schroeder@ni.com>

--- ni_measurement_plugin_sdk_service/discovery/_support.py
+++ ni_measurement_plugin_sdk_service/discovery/_support.py
@@ -57,7 +57,7 @@ def _get_discovery_service_location() -> pathlib.PurePath:
 
 def _get_registration_json_file_path() -> pathlib.Path:
     if sys.platform == "win32":
-        json_path = _get_nipath("NISHAREDDIR") / "Services" / "Services.json"
+        json_path = _get_nipath("NISHAREDDIR64") / "Services" / "Services.json"
         if not json_path.exists():
             json_path = (
                 _get_nipath("NISHAREDDIR64") / "MeasurementLink" / "MeasurementLinkServices.json"
