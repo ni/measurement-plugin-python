5c0f5cde8bad9bf95044d2a4dcd9b187d4b6df23
Jotheeswaran-Nandagopal <jotheeswaran.nandagopal@ni.com>
2024-09-18 23:20:11 +0530

    doc: update documentation for public class

--- packages/generator/tests/conftest.py
+++ packages/generator/tests/conftest.py
@@ -15,6 +15,7 @@ from tests.utilities.discovery_service_process import DiscoveryServiceProcess
 
 
 class CliRunnerFunction(Protocol):
+    """Protocol for a callable that executes a CLI command using Click's CliRunner."""
     def __call__(self, args: Sequence[str], input: Optional[str] = None) -> Result: ...
 
 
