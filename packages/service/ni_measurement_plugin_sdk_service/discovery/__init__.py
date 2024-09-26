b50741701eda5576bc39d603299d8016e4404e06
Jotheeswaran Nandagopal <jotheeswaran.nandagopal@ni.com>
2024-09-26 21:37:03 +0530

    fix: update import order

--- packages/service/ni_measurement_plugin_sdk_service/discovery/__init__.py
+++ packages/service/ni_measurement_plugin_sdk_service/discovery/__init__.py
@@ -2,8 +2,8 @@
 
 from ni_measurement_plugin_sdk_service.discovery._client import DiscoveryClient
 from ni_measurement_plugin_sdk_service.discovery._types import (
-    ServiceLocation,
     ServiceDetails,
+    ServiceLocation,
 )
 
 __all__ = ["DiscoveryClient", "ServiceLocation", "ServiceDetails"]
