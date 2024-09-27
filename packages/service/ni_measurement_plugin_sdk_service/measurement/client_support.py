aabda2119af45cf7e9accce527be904ac6adef62
Brad Keryan <brad.keryan@ni.com>
2024-09-27 09:33:03 -0500

    service: Update client_support to use type specialization constants

--- packages/service/ni_measurement_plugin_sdk_service/measurement/client_support.py
+++ packages/service/ni_measurement_plugin_sdk_service/measurement/client_support.py
@@ -5,6 +5,7 @@ from typing import Any, Dict, Sequence
 
 from google.protobuf.descriptor_pb2 import FieldDescriptorProto
 
+from ni_measurement_plugin_sdk_service._annotations import TYPE_SPECIALIZATION_KEY
 from ni_measurement_plugin_sdk_service._internal.parameter.decoder import (
     deserialize_parameters as _internal_deserialize_parameters,
 )
@@ -17,6 +18,7 @@ from ni_measurement_plugin_sdk_service._internal.parameter.metadata import (
 from ni_measurement_plugin_sdk_service._internal.parameter.serialization_descriptors import (
     create_file_descriptor,
 )
+from ni_measurement_plugin_sdk_service.measurement.info import TypeSpecialization
 
 __all__ = [
     "create_file_descriptor",
@@ -57,7 +59,7 @@ def deserialize_parameters(
             convert_paths
             and metadata.type == FieldDescriptorProto.TYPE_STRING
             and metadata.annotations
-            and metadata.annotations.get("ni/type_specialization") == "path"
+            and metadata.annotations.get(TYPE_SPECIALIZATION_KEY) == TypeSpecialization.Path.value
         ):
             if metadata.repeated:
                 parameter_values[id] = [Path(value) for value in parameter_values[id]]
@@ -100,7 +102,7 @@ def serialize_parameters(
         if (
             metadata.type == FieldDescriptorProto.TYPE_STRING
             and metadata.annotations
-            and metadata.annotations.get("ni/type_specialization") == "path"
+            and metadata.annotations.get(TYPE_SPECIALIZATION_KEY) == TypeSpecialization.Path.value
         ):
             if metadata.repeated:
                 new_parameter_values[index] = [str(value) for value in parameter_values[index]]
