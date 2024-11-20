7c2d26601804e6ddca23416524d935a4b09be7e3
Jayaseelan James <jayaseelan.james@ni.com>
2024-11-20 15:01:38 +0530

    example: rebrand proto and stubs for sample_measurement

--- examples/sample_measurement/_stubs/color_pb2_grpc.pyi
+++ examples/sample_measurement/_stubs/color_pb2_grpc.pyi
@@ -4,3 +4,16 @@ isort:skip_file
 ---------------------------------------------------------------------
 ---------------------------------------------------------------------
 """
+
+import abc
+import collections.abc
+import grpc
+import grpc.aio
+import typing
+
+_T = typing.TypeVar("_T")
+
+class _MaybeAsyncIterator(collections.abc.AsyncIterator[_T], collections.abc.Iterator[_T], metaclass=abc.ABCMeta): ...
+
+class _ServicerContext(grpc.ServicerContext, grpc.aio.ServicerContext):  # type: ignore[misc, type-arg]
+    ...
