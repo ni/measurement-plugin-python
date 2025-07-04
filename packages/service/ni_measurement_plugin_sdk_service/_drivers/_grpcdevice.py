f7385db84c242c93c7328e7d0145f9a88b24e3ed
divakar-selvam07 <divakar.selvam@emerson.com>
2025-07-04 14:59:34 +0530

    feat: populate deployment_target with remote node Url

--- packages/service/ni_measurement_plugin_sdk_service/_drivers/_grpcdevice.py
+++ packages/service/ni_measurement_plugin_sdk_service/_drivers/_grpcdevice.py
@@ -36,8 +36,13 @@ def get_grpc_device_server_location(
         )
         return service_location
 
+    compute_nodes = discovery_client.enumerate_compute_nodes()
+    remote_nodes = [node for node in compute_nodes if not node.is_local]
+    first_remote_node_url = remote_nodes[0].url if len(remote_nodes) == 1 else ""
+    
     service_location = discovery_client.resolve_service(
         provided_interface=provided_interface,
+        deployment_target=first_remote_node_url,
         service_class=SERVICE_CLASS,
     )
     _logger.debug(
