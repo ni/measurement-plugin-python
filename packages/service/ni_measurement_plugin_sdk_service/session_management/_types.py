bd3e4f4f521382c4003f39efa12e37b9a9aff886
Joel Dixon <joel.dixon@ni.com>
2025-08-18 16:48:36 -0500

    Fix mypy errors

--- packages/service/ni_measurement_plugin_sdk_service/session_management/_types.py
+++ packages/service/ni_measurement_plugin_sdk_service/session_management/_types.py
@@ -7,7 +7,7 @@ from enum import IntEnum
 from typing import Generic, NamedTuple, Protocol, TypeVar
 
 from ni_measurement_plugin_sdk_service._internal.stubs import session_pb2
-from ni_measurement_plugin_sdk_service._internal.stubs.ni.measurementlink import (
+from ni.measurementlink import (
     pin_map_context_pb2,
 )
 from ni_measurement_plugin_sdk_service._internal.stubs.ni.measurementlink.sessionmanagement.v1 import (
