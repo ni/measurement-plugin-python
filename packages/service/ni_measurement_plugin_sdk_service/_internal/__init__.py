cc383103c8c74a9ee01ecf5b05c076fab1bea19f
Michael Johansen <michael.johansen@ni.com>
2025-08-20 13:23:04 -0500

    Redirect old imports of ni.protobuf.types
    Signed-off-by: Michael Johansen <michael.johansen@ni.com>

--- packages/service/ni_measurement_plugin_sdk_service/_internal/__init__.py
+++ packages/service/ni_measurement_plugin_sdk_service/_internal/__init__.py
@@ -1,1 +1,7 @@
 """Internal modules and classes for Measurement Framework."""
+
+import sys
+
+import ni.protobuf
+
+sys.modules["ni_measurement_plugin_sdk_service._internal.stubs.ni.protobuf"] = ni.protobuf
