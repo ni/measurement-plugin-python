19e975fb4b862ab1ae934a4bf4a9da98c4d01354
Michael Johansen <michael.johansen@ni.com>
2025-08-20 13:25:31 -0500

    Add a comment to explain redirect.
    Signed-off-by: Michael Johansen <michael.johansen@ni.com>

--- packages/service/ni_measurement_plugin_sdk_service/_internal/__init__.py
+++ packages/service/ni_measurement_plugin_sdk_service/_internal/__init__.py
@@ -4,4 +4,6 @@ import sys
 
 import ni.protobuf
 
+
+# Redirect old imports of the stubs that used to be defined here to their new location.
 sys.modules["ni_measurement_plugin_sdk_service._internal.stubs.ni.protobuf"] = ni.protobuf
