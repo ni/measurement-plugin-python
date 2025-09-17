d38a2486d96b84de86fa92aa01fa7acd90b42980
Joel Dixon <joel.dixon@ni.com>
2025-09-17 17:23:58 -0500

    Update doc string

--- packages/service/ni_measurement_plugin_sdk_service/grpc/loggers/__init__.py
+++ packages/service/ni_measurement_plugin_sdk_service/grpc/loggers/__init__.py
@@ -1,4 +1,12 @@
-"""Redirects to the relocated ClientLogger."""
+"""Compatibility API for gRPC logging interceptors.
+
+The gRPC logging interceptor classes have moved to the :mod:`ni_grpc_extensions`
+package.
+
+The :mod:`ni_measurement_plugin_sdk_service.grpc.loggers` submodule provides
+compatibility with existing applications and will be deprecated in a future
+release.
+"""
 
 from ni_grpc_extensions.loggers import ClientLogger, ServerLogger
 
