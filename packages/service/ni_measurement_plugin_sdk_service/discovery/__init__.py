3cb30455e857b1537ab1210a2598dfd239384c8a
Joel Dixon <joel.dixon@ni.com>
2025-09-17 17:14:01 -0500

    Remove ServiceInfo export from discovery module

--- packages/service/ni_measurement_plugin_sdk_service/discovery/__init__.py
+++ packages/service/ni_measurement_plugin_sdk_service/discovery/__init__.py
@@ -1,5 +1,5 @@
 """Redirects to the relocated DiscoveryClient."""
 
-from ni.measurementlink.discovery.v1.client import DiscoveryClient, ServiceLocation, ServiceInfo
+from ni.measurementlink.discovery.v1.client import DiscoveryClient, ServiceLocation
 
-__all__ = ["DiscoveryClient", "ServiceLocation", "ServiceInfo"]
+__all__ = ["DiscoveryClient", "ServiceLocation"]
