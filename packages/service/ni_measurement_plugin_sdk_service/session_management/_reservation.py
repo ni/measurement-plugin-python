af8d3f55ad221819f00d809f1ae8afdac36d6cea
Joel Dixon <joel.dixon@ni.com>
2025-10-06 12:54:04 -0500

    Restore more exports from _reservation and _types

--- packages/service/ni_measurement_plugin_sdk_service/session_management/_reservation.py
+++ packages/service/ni_measurement_plugin_sdk_service/session_management/_reservation.py
@@ -1,3 +1,14 @@
 from ni.measurementlink.sessionmanagement.v1.client import MultiSessionReservation
+from ni.measurementlink.sessionmanagement.v1.client import (
+    BaseReservation,
+    MultiSessionReservation,
+    MultiplexerSessionContainer,
+    SingleSessionReservation,
+)
 
-__all__ = ["MultiSessionReservation"]
+__all__ = [
+    "BaseReservation",
+    "MultiplexerSessionContainer",
+    "MultiSessionReservation",
+    "SingleSessionReservation",
+]
--- packages/service/ni_measurement_plugin_sdk_service/session_management/_types.py
+++ packages/service/ni_measurement_plugin_sdk_service/session_management/_types.py
@@ -1,3 +1,27 @@
-from ni.measurementlink.sessionmanagement.v1.client import SessionInitializationBehavior
+from ni.measurementlink.sessionmanagement.v1.client import (
+    ChannelMapping,
+    Connection,
+    MultiplexerSessionInformation,
+    PinMapContext,
+    SessionInformation,
+    SessionInitializationBehavior,
+    TypedConnection,
+    TypedConnectionWithMultiplexer,
+    TypedMultiplexerSessionInformation,
+    TypedSessionInformation,
+)
 
-__all__ = ["SessionInitializationBehavior"]
+__all__ = [
+    "ChannelMapping",
+    "Connection",
+    "MultiplexerSessionInformation",
+    "PinMapContext",
+    "SessionInformation",
+    "SessionInitializationBehavior",
+    "TypedConnection",
+    "TypedConnectionWithMultiplexer",
+    "TypedMultiplexerSessionInformation",
+    "TypedSessionInformation",
+]
+
+
