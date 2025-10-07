8d5dda4a6e0463ba1be9ff2da68d77e78e40ae42
Joel Dixon <38357562+dixonjoel@users.noreply.github.com>
2025-10-07 09:20:08 -0500

    [Cherry pick from releases/3.0] Restore session_management module export (#1253)
    Restore session_management module export to service package (#1250)
    
    * Update examples to >=2.3.1,<4.0
    
    * Restore session_management export from ni_measurement_plugin_sdk_service
    
    * Set version numbers to 3.0.1
    
    * Restore _types module with SessionInitializationBehavior
    
    * Do not reference private modules from ni_measurement_plugin_sdk_service.
    
    * Revert version change so the rest can be cherry picked to main
    
    * Restore more exports from _reservation and _types
    
    * Fix linter errors

--- packages/service/ni_measurement_plugin_sdk_service/session_management/_reservation.py
+++ packages/service/ni_measurement_plugin_sdk_service/session_management/_reservation.py
@@ -1,3 +1,13 @@
-from ni.measurementlink.sessionmanagement.v1.client import MultiSessionReservation
+from ni.measurementlink.sessionmanagement.v1.client import (
+    BaseReservation,
+    MultiplexerSessionContainer,
+    MultiSessionReservation,
+    SingleSessionReservation,
+)
 
-__all__ = ["MultiSessionReservation"]
+__all__ = [
+    "BaseReservation",
+    "MultiplexerSessionContainer",
+    "MultiSessionReservation",
+    "SingleSessionReservation",
+]
---
+++ packages/service/ni_measurement_plugin_sdk_service/session_management/_types.py
@@ -0,0 +1,25 @@
+from ni.measurementlink.sessionmanagement.v1.client import (
+    ChannelMapping,
+    Connection,
+    MultiplexerSessionInformation,
+    PinMapContext,
+    SessionInformation,
+    SessionInitializationBehavior,
+    TypedConnection,
+    TypedConnectionWithMultiplexer,
+    TypedMultiplexerSessionInformation,
+    TypedSessionInformation,
+)
+
+__all__ = [
+    "ChannelMapping",
+    "Connection",
+    "MultiplexerSessionInformation",
+    "PinMapContext",
+    "SessionInformation",
+    "SessionInitializationBehavior",
+    "TypedConnection",
+    "TypedConnectionWithMultiplexer",
+    "TypedMultiplexerSessionInformation",
+    "TypedSessionInformation",
+]
