fdf1cc5a97b93577a676345484c03e744838fbbe
Brad Keryan <brad.keryan@ni.com>
2026-06-05 17:06:57 -0500

    docs: Use intersphinx to link to ni-apis-python documentation (#1309)

--- packages/service/ni_measurement_plugin_sdk_service/grpc/__init__.py
+++ packages/service/ni_measurement_plugin_sdk_service/grpc/__init__.py
@@ -1,7 +1,6 @@
 """Compatibility API for gRPC extensions.
 
-The public gRPC extensions API has moved to the :mod:`ni_grpc_extensions`
-package.
+The public gRPC extensions API has moved to the :external+ni_grpc_extensions:doc:`index` package.
 
 The :mod:`ni_measurement_plugin_sdk_service.grpc` subpackage provides
 compatibility with existing applications and will be deprecated in a future
--- packages/service/ni_measurement_plugin_sdk_service/grpc/channelpool.py
+++ packages/service/ni_measurement_plugin_sdk_service/grpc/channelpool.py
@@ -1,7 +1,7 @@
 """Compatibility API for gRPC channel pool.
 
-The :obj:`GrpcChannelPool` class has moved to the :mod:`ni_grpc_extensions`
-package.
+The :obj:`GrpcChannelPool` class has moved to the :mod:`ni_grpc_extensions.channelpool`
+submodule.
 
 The :mod:`ni_measurement_plugin_sdk_service.grpc.channelpool` submodule provides
 compatibility with existing applications and will be deprecated in a future
